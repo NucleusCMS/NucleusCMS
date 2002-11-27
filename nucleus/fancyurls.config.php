7db8199d2b561a0a75957c640c78d8046c17e06b
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2002-11-27 18:05:15 +0000

    fancy urls
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@16 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

---
+++ nucleus/fancyurls.config.php
@@ -0,0 +1,29 @@
+<?
+	/*
+		About
+		-----
+		This directory contains extra files to make the 'fancy urls' feature even more
+		fancier, by eliminating the 'index.php'-part of the URL
+	
+		Installation
+		------------
+		
+		1. Copy all files in this directory (except for index.html) to your main nucleus dir
+		   (where your index.php and action.php file are)
+		2. Edit this file so that $CONF['Self'] points to your main directory. 
+			NOTE: this time, and only this time, the URL should NOT end in a slash
+		3. Enable 'Fancy URLs' in the Nucleus admin area (nucleus management / edit settings)
+		4. Off you go!
+		
+		If it doesn't work:
+		-------------------
+		
+		Remove the files again (don't forget the hidden file .htaccess). Voila.
+		
+	*/
+
+	
+	// remember: this URL should _NOT_ end with a slash. 
+	$CONF['Self'] = 'http://www.yourhost.com/yourpath';
+	
+?>
