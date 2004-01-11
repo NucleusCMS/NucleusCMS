8427a53bd7619a6d18c317a757288b04768a2620
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2004-01-11 10:26:35 +0000

    added GPL header
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@368 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/xmlrpc/api_blogger.inc.php
+++ nucleus/xmlrpc/api_blogger.inc.php
@@ -1,7 +1,18 @@
 <?php
-	/*
-	 * This file contains definitions for the methods in the Blogger API
-	 */
+/** 
+  * Nucleus: PHP/MySQL Weblog CMS (http://nucleuscms.org/) 
+  * Copyright (C) 2002-2004 The Nucleus Group
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation; either version 2
+  * of the License, or (at your option) any later version.
+  * (see nucleus/documentation/index.html#license for more info)
+  */
+  
+/*
+ * This file contains definitions for the methods in the Blogger API
+ */
 
 
 	// blogger.newPost
--- nucleus/xmlrpc/api_mt.inc.php
+++ nucleus/xmlrpc/api_mt.inc.php
@@ -1,9 +1,21 @@
 <?php
-	/*
-	 * This file contains definitions for the methods in the Movable Type API
-	 *
-	 * Wouter Demuynck 2003-08-31
-	 */
+
+/** 
+  * Nucleus: PHP/MySQL Weblog CMS (http://nucleuscms.org/) 
+  * Copyright (C) 2002-2004 The Nucleus Group
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation; either version 2
+  * of the License, or (at your option) any later version.
+  * (see nucleus/documentation/index.html#license for more info)
+  */
+
+/*
+ * This file contains definitions for the methods in the Movable Type API
+ *
+ * Wouter Demuynck 2003-08-31
+ */
 
 
 	// mt.supportedMethods
