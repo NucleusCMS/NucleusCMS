b1f409c1fe545a41493661014bea542bd270b20e
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2004-01-11 10:27:05 +0000

    metaWeblog.getPost/getRecentPosts: Added 'postid' to response struct
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@369 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/xmlrpc/api_metaweblog.inc.php
+++ nucleus/xmlrpc/api_metaweblog.inc.php
@@ -1,6 +1,19 @@
-<?php	/*
-	 *	This file contains definitions for the methods of the metaWeblog API
-	 */
+<?php
+
+/** 
+  * Nucleus: PHP/MySQL Weblog CMS (http://nucleuscms.org/) 
+  * Copyright (C) 2002-2004 The Nucleus Group
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation; either version 2
+  * of the License, or (at your option) any later version.
+  * (see nucleus/documentation/index.html#license for more info)
+  */
+
+/*
+ *	This file contains definitions for the methods of the metaWeblog API
+ */
 	 
 	
 	// metaWeblog.newPost
@@ -217,6 +230,7 @@
 				"dateCreated" => new xmlrpcval(iso8601_encode($row['itime']),"dateTime.iso8601"),
 				"userid" => new xmlrpcval($row['iauthor'],"string"),
 				"blogid" => new xmlrpcval($blogid,"string"),
+				"postid" => new xmlrpcval($row['inumber'],"string"),
 				"description" => new xmlrpcval($row['ibody'],"string"),
 				"title" => new xmlrpcval($row['title'],"string"),
 				"categories" => new xmlrpcval(
@@ -225,6 +239,13 @@
 						)
 						,"array")
 			),'struct');
+			
+			"postid" => new xmlrpcval($itemid, "string"),
+		
+		//TODO: String link?
+		//TODO: String permaLink?
+		//TODO: String nucleus_more?
+		
 		
 			array_push($structarray, $newstruct);		
 		}
@@ -355,6 +376,7 @@
 			"dateCreated" => new xmlrpcval(iso8601_encode($item['timestamp']),"dateTime.iso8601"),
 			"userid" => new xmlrpcval($item['authorid'],"string"),
 			"blogid" => new xmlrpcval($blogid,"string"),
+			"postid" => new xmlrpcval($itemid, "string"),
 			"description" => new xmlrpcval($item['body'],"string"),
 			"title" => new xmlrpcval($item['title'],"string"),
 			"categories" => new xmlrpcval(
@@ -363,6 +385,10 @@
 					)
 					,"array")
 		),'struct');
+		
+		//TODO: add "String link" to struct?
+		//TODO: add "String permaLink" to struct? 
+		//TODO: add "String nucleus_more" to struct?
 
 		return new xmlrpcresp($newstruct);
 	
