6fc67ab11fbe5756c655b4adb1bea55f9b09255c
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2004-03-15 17:27:37 +0000

    CHANGED: Allowed skin names to have slashes ("/") in them
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@395 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/nucleus/libs/globalfunctions.php
+++ nucleus/nucleus/libs/globalfunctions.php
@@ -652,9 +652,9 @@ function getConfig() {
 // some checks for names of blogs, categories, templates, members, ...
 function isValidShortName($name) {		return eregi('^[a-z0-9]+$', $name); }
 function isValidDisplayName($name) {	return eregi('^[a-z0-9]+[a-z0-9 ]*[a-z0-9]+$', $name); }
-function isValidCategoryName($name) {	return 1; } // return eregi('^[a-zA-Z0-9 ]+$', $name); }
+function isValidCategoryName($name) {	return 1; } 
 function isValidTemplateName($name) {	return eregi('^[a-z0-9/]+$', $name); }
-function isValidSkinName($name) {		return isValidShortName($name); }
+function isValidSkinName($name) {		return eregi('^[a-z0-9/]+$', $name); }
 
 // add and remove linebreaks
 function addBreaks($var) { 				return nl2br($var); }
