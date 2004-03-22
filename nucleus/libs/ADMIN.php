a79fb84bfd857081d501f39e809ed5395863c9fd
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2004-03-22 19:17:58 +0000

    oops
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@412 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/libs/ADMIN.php
+++ nucleus/libs/ADMIN.php
@@ -1556,6 +1556,7 @@ class ADMIN {
 		<?php if ($CONF['AllowLoginEdit'] || $member->isAdmin()) { ?>
 			<td><?php echo _MEMBERS_PWD?></td>
 			<td><input type="password" tabindex="30" maxlength="40" size="16" name="password" /></td>
+		<?php } ?>
 		</tr><tr>
 			<td><?php echo _MEMBERS_REPPWD?></td>
 			<td><input type="password" tabindex="35" maxlength="40" size="16" name="repeatpassword" /></td>
