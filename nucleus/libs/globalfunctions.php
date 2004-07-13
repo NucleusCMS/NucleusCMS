7d4c9c68c46b6c4f637dd1d0f18287055e61aa9e
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2004-07-13 17:57:00 +0000

    some unchecked variables
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@506 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/libs/globalfunctions.php
+++ nucleus/libs/globalfunctions.php
@@ -13,7 +13,10 @@
   */
 
 // needed if we include globalfunctions from install.php
-global $nucleus, $CONF, $DIR_LIBS, $DIR_LANG, $manager, $member;
+global $nucleus, $CONF, $DIR_LIBS, $DIR_LANG, $manager, $member; 
+
+
+checkVars(array('nucleus', 'CONF', 'DIR_LIBS', 'MYSQL_HOST', 'MYSQL_USER', 'MYSQL_PASSWORD', 'MYSQL_DATABASE', 'DIR_LANG', 'DIR_PLUGINS'));
 
 $nucleus['version'] = 'v3.0+ CVS';
 $CONF['debug'] = 1;
@@ -1012,53 +1015,21 @@ function formatDate($format, $timestamp, $defaultFormat) {
 
 }
 
-/*
-  These functions create a FancierURL item link, needs NP_FancierURL plugin
-  installed. I (radek) put it here, to make it possible for all plugins to support 
-  FancierURL functionality.
-
-  karma: do we really need this? 
-
-
-// create a FancierURL link from ID 
-function fancyLink($id) {
-  if (empty($id)) return '';
-	global $CONF;
-	if ($CONF['URLMode'] == 'pathinfo') {
-	  // fancier URLs
-	  $query = mysql_query('SELECT iurltitle, UNIX_TIMESTAMP(itime) as itime FROM '.sql_table('item').' WHERE inumber='.strval($id));
-	  if ($obj = mysql_fetch_object($query)){
-	    $r=fancyTitle($id,$obj->iurltitle,$obj->itime);
-     } else {
-       $r='';
-     }
-     return $r;
-   } else {
-     // normal URLs
-      return $CONF['IndexURL'].'?itemid='.(strval($id));
-   }
+function checkVars($aVars)
+{
+	global $HTTP_GET_VARS, $HTTP_POST_VARS, $HTTP_COOKIE_VARS, $HTTP_ENV_VARS, $HTTP_POST_FILES, $HTTP_SESSION_VARS;
+	foreach ($aVars as $varName)
+	{
+		if (   isset($HTTP_GET_VARS[$varName]) 
+			|| isset($HTTP_POST_VARS[$varName]) 
+			|| isset($HTTP_COOKIE_VARS[$varName])
+			|| isset($HTTP_ENV_VARS[$varName])
+			|| isset($HTTP_SESSION_VARS[$varName])
+			|| isset($HTTP_POST_FILES[$varName])
+		){
+			die('Sorry. An error occurred.');
+		}
+	}
 }
-
-// format a FancierURL link 
-function fancyTitle($id,$title,$date) {
-  if (empty($id)) return '';
-	global $CONF;
-	if ($CONF['URLMode'] == 'pathinfo') {
-	  // fancier URLs
-    $date = getdate($date);
-    if (isset($CONF['ItemURL']) && preg_match('/^http:\/\//', $CONF['ItemURL'])) {
-	    $self = $CONF['ItemURL'] . '/';
-    } else {
-	    $self = $CONF['IndexURL'];
-    }
-    $link = $self.'item/'.$title;
-    return $link;
-  } else {
-    // normal URLs
-    return $CONF['IndexURL'].'?itemid='.(strval($id));
-  }
-}
-
-*/
 
 ?>
