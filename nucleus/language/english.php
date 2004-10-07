51a2ee54945ce2c146080c646338114fa7e023cc
dekarma <dekarma@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2004-10-07 16:37:40 +0000

    Text for <%arcihvetype%> was not in language files
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@620 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/language/english.php
+++ nucleus/language/english.php
@@ -14,6 +14,10 @@
 
 // START changed/added after 3.1 START
 
+// for use in archivetype-skinvar
+define('_ARCHIVETYPE_DAY',			'day');
+define('_ARCHIVETYPE_MONTH',		'month');
+
 // tickets (prevents malicious users to trick an admin to perform actions he doesn't want)
 define('_ERROR_BADTICKET',			'Invalid or expired ticket.');
 
