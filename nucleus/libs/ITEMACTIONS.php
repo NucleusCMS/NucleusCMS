e1b2294078c4f5ecaf151df524f99c9f5cc40600
rodrigomoraes <rodrigomoraes@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2005-12-12 00:37:43 +0000

    added license.
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@838 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/libs/ITEMACTIONS.php
+++ nucleus/libs/ITEMACTIONS.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * Nucleus: PHP/MySQL Weblog CMS (http://nucleuscms.org/)
+ * Copyright (C) 2002-2005 The Nucleus Group
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * (see nucleus/documentation/index.html#license for more info)
+ */
 
 /**
   * This class is used when parsing item templates
