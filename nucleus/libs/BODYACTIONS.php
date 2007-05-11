b62c535e6afa00445abe7cd97df14cb84cfc53f8
kimitake <kimitake@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2007-05-11 08:26:02 +0000

    (Rev 1.5) Security fix, added ENT_QUOTES to htmlspecialchars function.
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@1147 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/libs/BODYACTIONS.php
+++ nucleus/libs/BODYACTIONS.php
@@ -81,8 +81,8 @@ class BODYACTIONS extends BaseActions {
 		$windowwidth = $width;
 		$windowheight = $height;
 
-		$vars['link']			= htmlspecialchars($CONF['MediaURL']. $filename);
-		$vars['text']			= htmlspecialchars($text);
+		$vars['link']			= htmlspecialchars($CONF['MediaURL']. $filename ,ENT_QUOTES);
+		$vars['text']			= htmlspecialchars($text ,ENT_QUOTES);
 		$vars['image'] = '<img src="' . $vars['link'] . '" width="' . $width . '" height="' . $height . '" alt="' . $vars['text'] . '" title="' . $vars['text'] . '" />';
 		$vars['width'] 			= $width;
 		$vars['height']			= $height;
@@ -108,8 +108,8 @@ class BODYACTIONS extends BaseActions {
 			$filename = $this->currentItem->authorid . '/' . $filename;
 		}
 
-		$vars['link']			= htmlspecialchars($CONF['MediaURL'] . $filename);
-		$vars['text']			= htmlspecialchars($text);
+		$vars['link']			= htmlspecialchars($CONF['MediaURL'] . $filename ,ENT_QUOTES);
+		$vars['text']			= htmlspecialchars($text ,ENT_QUOTES);
 		$vars['media'] 			= '<a href="' . $vars['link'] . '">' . $vars['text'] . '</a>';
 
 		echo TEMPLATE::fill($this->template['MEDIA_CODE'],$vars);;
@@ -134,14 +134,14 @@ class BODYACTIONS extends BaseActions {
 		$windowwidth = $width;
 		$windowheight = $height;
 
-		$vars['rawpopuplink'] 	= $CONF['Self'] . "?imagepopup=" . htmlspecialchars($filename) . "&amp;width=$width&amp;height=$height&amp;imagetext=" . urlencode(htmlspecialchars($text));
+		$vars['rawpopuplink'] 	= $CONF['Self'] . "?imagepopup=" . htmlspecialchars($filename,ENT_QUOTES) . "&amp;width=$width&amp;height=$height&amp;imagetext=" . urlencode(htmlspecialchars($text));
 		$vars['popupcode'] 		= "window.open(this.href,'imagepopup','status=no,toolbar=no,scrollbars=no,resizable=yes,width=$windowwidth,height=$windowheight');return false;";
-		$vars['popuptext'] 		= htmlspecialchars($text);
+		$vars['popuptext'] 		= htmlspecialchars($text,ENT_QUOTES);
 		$vars['popuplink'] 		= '<a href="' . $vars['rawpopuplink']. '" onclick="'. $vars['popupcode'].'" >' . $vars['popuptext'] . '</a>';
 		$vars['width'] 			= $width;
 		$vars['height']			= $height;
 		$vars['text']			= $text;
-		$vars['link']			= htmlspecialchars($CONF['MediaURL'] . $filename);
+		$vars['link']			= htmlspecialchars($CONF['MediaURL'] . $filename ,ENT_QUOTES);
 		$vars['media'] 			= '<a href="' . $vars['link'] . '">' . $vars['popuptext'] . '</a>';
 
 		echo TEMPLATE::fill($this->template['POPUP_CODE'],$vars);
