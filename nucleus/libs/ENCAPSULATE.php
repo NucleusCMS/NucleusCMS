4053c1af921c865b6940473d6bbc4a854fd136e3
kaigreve <kaigreve@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2007-05-20 23:24:09 +0000

    Security fix from Nucleus Japan (revision 1.4)
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk/nucleus@1154 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/libs/ENCAPSULATE.php
+++ nucleus/libs/ENCAPSULATE.php
@@ -94,7 +94,7 @@ class NAVLIST extends ENCAPSULATE {
 		$minamount = $this->minamount;
 		$maxamount = $this->maxamount;
 		$blogid = $this->blogid;
-		$search = $this->search;
+		$search = htmlspecialchars($this->search);
 		$itemid = $this->itemid;
 
 		$prev = $start - $amount;
