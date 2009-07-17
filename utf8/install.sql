292717bda6a0f746cfcccdfa38ab618f0492fdbe
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2009-07-17 02:31:49 +0000

    新規インストール時の複数スキンインストール対応(install.php) `bsendping`のフラグの削除(install.sql) Parameter n to function() expected to be a reference問題修正(nucleus/plugins/*/php)
    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/nucleus-jp/trunk@1036 1ca29b6e-896d-4ea0-84a5-967f57386b96

--- utf8/install.sql
+++ utf8/install.sql
@@ -16,8 +16,6 @@ CREATE TABLE `nucleus_ban` (
   `iprange` varchar(15)  NOT NULL default '',
   `reason`  varchar(255) NOT NULL default '',
   `blogid`  int(11)      NOT NULL default '0'
---  KEY `iprange` (`iprange`),
---  KEY `blogid`  (`blogid`)
 ) TYPE=MyISAM;
 
 CREATE TABLE `nucleus_blog` (
@@ -33,7 +31,6 @@ CREATE TABLE `nucleus_blog` (
   `bupdate`        varchar(60)           default NULL,
   `bdefskin`       int(11)      NOT NULL default '1',
   `bpublic`        tinyint(2)   NOT NULL default '1',
---  `bsendping`      tinyint(2)   NOT NULL default '0',
   `bconvertbreaks` tinyint(2)   NOT NULL default '1',
   `bdefcat`        int(11)               default NULL,
   `bnotifytype`    int(11)      NOT NULL default '15',
@@ -42,11 +39,30 @@ CREATE TABLE `nucleus_blog` (
   `breqemail`      tinyint(2)   NOT NULL default '0',
   `bfuturepost`    tinyint(2)   NOT NULL default '0',
   PRIMARY KEY  (`bnumber`),
--- UNIQUE KEY `bnumber` (`bnumber`),
   UNIQUE KEY `bshortname` (`bshortname`)
 ) TYPE=MyISAM;
 
-INSERT INTO `nucleus_blog` VALUES (1, 'My Nucleus CMS', 'mynucleuscms', '', 1, 0, 0.0, '', 'http://localhost:8080/nucleus/', '', 5, 1, 0, 1, 1, 1, 1, 0, 0, 0);
+INSERT INTO `nucleus_blog` VALUES (
+    1,                                  /* bnumber */
+    'My Nucleus CMS',                   /* bname */
+    'mynucleuscms',                     /* bshortname */
+    '',                                 /* bdesc */
+    1,                                  /* bcomments */
+    0,                                  /* bmaxcomments */
+    0.0,                                /* btimeoffset */
+    '',                                 /* bnotify */
+    'http://localhost:8080/nucleus/',   /* burl */
+    '',                                 /* bupdate */
+    5,                                  /* bdefskin */
+    1,                                  /* bpublic */
+    1,                                  /* bconvertbreaks */
+    1,                                  /* bdefcat */
+    1,                                  /* bnotifytype */
+    1,                                  /* ballowpast */
+    0,                                  /* bincludesearch */
+    0,                                  /* breqemail */
+    0                                   /* bfuturepost */
+);
 
 CREATE TABLE `nucleus_category` (
   `catid` int(11) NOT NULL auto_increment,
@@ -71,7 +87,6 @@ CREATE TABLE `nucleus_comment` (
   `cip`     varchar(15)  NOT NULL default '',
   `cblog`   int(11)      NOT NULL default '0',
   PRIMARY KEY  (`cnumber`),
---  UNIQUE KEY `cnumber` (`cnumber`),
   KEY `citem` (`citem`),
   FULLTEXT KEY `cbody` (`cbody`)
 ) TYPE=MyISAM;
@@ -82,40 +97,41 @@ CREATE TABLE `nucleus_config` (
   PRIMARY KEY  (`name`)
 ) TYPE=MyISAM;
 
-INSERT INTO `nucleus_config` VALUES ('DefaultBlog',       '1');
-INSERT INTO `nucleus_config` VALUES ('AdminEmail',        'example@example.org');
-INSERT INTO `nucleus_config` VALUES ('IndexURL',          'http://localhost:8080/nucleus/');
-INSERT INTO `nucleus_config` VALUES ('Language',          'japanese-utf8');
-INSERT INTO `nucleus_config` VALUES ('SessionCookie',     '');
-INSERT INTO `nucleus_config` VALUES ('AllowMemberCreate', '');
-INSERT INTO `nucleus_config` VALUES ('AllowMemberMail',   '1');
-INSERT INTO `nucleus_config` VALUES ('SiteName',          'My Nucleus CMS');
-INSERT INTO `nucleus_config` VALUES ('AdminURL',          'http://localhost:8080/nucleus/nucleus/');
-INSERT INTO `nucleus_config` VALUES ('NewMemberCanLogon', '1');
-INSERT INTO `nucleus_config` VALUES ('DisableSite',       '');
-INSERT INTO `nucleus_config` VALUES ('DisableSiteURL',    'http://www.this-page-intentionally-left-blank.org/');
-INSERT INTO `nucleus_config` VALUES ('LastVisit',         '');
-INSERT INTO `nucleus_config` VALUES ('MediaURL',          'http://localhost:8080/nucleus/media/');
-INSERT INTO `nucleus_config` VALUES ('AllowedTypes',      'jpg,jpeg,gif,mpg,mpeg,avi,mov,mp3,swf,png');
-INSERT INTO `nucleus_config` VALUES ('AllowLoginEdit',    '');
-INSERT INTO `nucleus_config` VALUES ('AllowUpload',       '1');
-INSERT INTO `nucleus_config` VALUES ('DisableJsTools',    '2');
-INSERT INTO `nucleus_config` VALUES ('CookiePath',        '/');
-INSERT INTO `nucleus_config` VALUES ('CookieDomain',      '');
-INSERT INTO `nucleus_config` VALUES ('CookieSecure',      '');
-INSERT INTO `nucleus_config` VALUES ('CookiePrefix',      '');
-INSERT INTO `nucleus_config` VALUES ('MediaPrefix',       '1');
-INSERT INTO `nucleus_config` VALUES ('MaxUploadSize',     '1048576');
-INSERT INTO `nucleus_config` VALUES ('NonmemberMail',     '');
-INSERT INTO `nucleus_config` VALUES ('PluginURL',         'http://localhost:8080/nucleus/nucleus/plugins/');
-INSERT INTO `nucleus_config` VALUES ('ProtectMemNames',   '1');
-INSERT INTO `nucleus_config` VALUES ('BaseSkin',          '5');
-INSERT INTO `nucleus_config` VALUES ('SkinsURL',          'http://localhost:8080/nucleus/skins/');
-INSERT INTO `nucleus_config` VALUES ('ActionURL',         'http://localhost:8080/nucleus/action.php');
-INSERT INTO `nucleus_config` VALUES ('URLMode',           'normal');
-INSERT INTO `nucleus_config` VALUES ('DatabaseVersion',   '340');
-INSERT INTO `nucleus_config` VALUES ('DebugVars',         '0');
-INSERT INTO `nucleus_config` VALUES ('DefaultListSize',   '10');
+INSERT INTO `nucleus_config` (`name`, `value`) VALUES
+    ('DefaultBlog',       '1'),
+    ('AdminEmail',        'example@example.org'),
+    ('IndexURL',          'http://localhost:8080/nucleus/'),
+    ('Language',          'japanese-utf8'),
+    ('SessionCookie',     ''),
+    ('AllowMemberCreate', ''),
+    ('AllowMemberMail',   '1'),
+    ('SiteName',          'My Nucleus CMS'),
+    ('AdminURL',          'http://localhost:8080/nucleus/nucleus/'),
+    ('NewMemberCanLogon', '1'),
+    ('DisableSite',       ''),
+    ('DisableSiteURL',    'http://www.this-page-intentionally-left-blank.org/'),
+    ('LastVisit',         ''),
+    ('MediaURL',          'http://localhost:8080/nucleus/media/'),
+    ('AllowedTypes',      'jpg,jpeg,gif,mpg,mpeg,avi,mov,mp3,swf,png'),
+    ('AllowLoginEdit',    ''),
+    ('AllowUpload',       '1'),
+    ('DisableJsTools',    '2'),
+    ('CookiePath',        '/'),
+    ('CookieDomain',      ''),
+    ('CookieSecure',      ''),
+    ('CookiePrefix',      ''),
+    ('MediaPrefix',       '1'),
+    ('MaxUploadSize',     '1048576'),
+    ('NonmemberMail',     ''),
+    ('PluginURL',         'http://localhost:8080/nucleus/nucleus/plugins/'),
+    ('ProtectMemNames',   '1'),
+    ('BaseSkin',          '5'),
+    ('SkinsURL',          'http://localhost:8080/nucleus/skins/'),
+    ('ActionURL',         'http://localhost:8080/nucleus/action.php'),
+    ('URLMode',           'normal'),
+    ('DatabaseVersion',   '340'),
+    ('DebugVars',         '0'),
+    ('DefaultListSize',   '10');
 
 CREATE TABLE `nucleus_item` (
   `inumber`   int(11)      NOT NULL auto_increment,
@@ -132,7 +148,6 @@ CREATE TABLE `nucleus_item` (
   `ikarmaneg` int(11)      NOT NULL default '0',
   `iposted`   tinyint(2)   NOT NULL default '1',
   PRIMARY KEY  (`inumber`),
---  UNIQUE KEY `inumber` (`inumber`),
   KEY `itime` (`itime`),
   FULLTEXT KEY `ibody` (`ibody`, `ititle`, `imore`)
 ) TYPE=MyISAM PACK_KEYS=0;
@@ -140,7 +155,6 @@ CREATE TABLE `nucleus_item` (
 CREATE TABLE `nucleus_karma` (
   `itemid` int(11)  NOT NULL default '0',
   `ip`     char(15) NOT NULL default ''
---,  KEY      `itemid` (`itemid`)
 ) TYPE=MyISAM;
 
 CREATE TABLE `nucleus_member` (
@@ -157,18 +171,29 @@ CREATE TABLE `nucleus_member` (
   `deflang`    varchar(20)  NOT NULL default '',
   `mautosave`  tinyint(2)   NOT NULL default '1',
   PRIMARY KEY         (`mnumber`),
---  UNIQUE KEY `mnumber` (`mnumber`),
   UNIQUE  KEY `mname` (`mname`)
 ) TYPE=MyISAM;
 
-INSERT INTO `nucleus_member` VALUES (1, 'example', 'example', '1a79a4d60de6718e8e5b326e338ae533', 'example@example.org', 'http://localhost:8080/nucleus/', '', 1, 1, 'd767aefc60415859570d64c649257f19', '', 1);
+INSERT INTO `nucleus_member` VALUES (
+    1,                                  /* mnumber */
+    'example',                          /* mname */
+    'example',                          /* mrealname */
+    '1a79a4d60de6718e8e5b326e338ae533', /* mpassword */
+    'example@example.org',              /* memail */
+    'http://localhost:8080/nucleus/',   /* murl */
+    '',                                 /* mnotes */
+    1,                                  /* madmin */
+    1,                                  /* mcanlogin */
+    'd767aefc60415859570d64c649257f19', /* mcookiekey */
+    '',                                 /* deflang */
+    1                                   /* mautosave */
+);
 
 CREATE TABLE `nucleus_plugin` (
   `pid`    int(11)     NOT NULL auto_increment,
   `pfile`  varchar(40) NOT NULL default '',
   `porder` int(11)     NOT NULL default '0',
   PRIMARY KEY     (`pid`),
---  KEY `pid` (`pid`),
   KEY    `porder` (`porder`)
 ) TYPE=MyISAM;
 
@@ -205,10 +230,6 @@ CREATE TABLE `nucleus_skin` (
   PRIMARY KEY  (`sdesc`,`stype`)
 ) TYPE=MyISAM;
 
-INSERT INTO `nucleus_skin` VALUES (2, 'index', '<?xml version="1.0" encoding="UTF-8"?>\n\n<feed xml:lang="ja" xmlns="http://www.w3.org/2005/Atom\">\n    <title><%blogsetting(name)%></title>\n    <subtitle><%blogsetting(desc)%></subtitle>\n    <id><%blogsetting(url)%>:<%blogsetting(id)%></id>\n\n    <link rel="alternate" type="text/html" href="<%blogsetting(url)%>" />\n    <link rel="self" type="application/atom+xml" href="<%blogsetting(url)%><%self%>" />\n    <generator uri="http://nucleuscms.org/"><%version%></generator>\n    <updated><%blog(feeds/atom/modified,1)%></updated>\n\n    <%blog(feeds/atom/entries,10)%>\n</feed>');
-INSERT INTO `nucleus_skin` VALUES (4, 'index', '<?xml version="1.0"?>\r\n<rsd version="1.0">\r\n <service>\r\n  <engineName><%version%></engineName>\r\n  <engineLink>http://nucleuscms.org/</engineLink>\r\n  <homepageLink><%sitevar(url)%></homepageLink>\r\n  <apis>\r\n   <api name="MetaWeblog" preferred="true" apiLink="<%adminurl%>xmlrpc/server.php" blogID="<%blogsetting(id)%>">\r\n    <docs>http://nucleuscms.org/documentation/devdocs/xmlrpc.html</docs>\r\n   </api>\r\n   <api name="Blogger" preferred="false" apiLink="<%adminurl%>xmlrpc/server.php" blogID="<%blogsetting(id)%>">\r\n    <docs>http://nucleuscms.org/documentation/devdocs/xmlrpc.html</docs>\r\n   </api>\r\n  </apis>\r\n </service>\r\n</rsd>');
-INSERT INTO `nucleus_skin` VALUES (3, 'index', '<?xml version="1.0" encoding="UTF-8"?>\r\n<rss version="2.0">\r\n  <channel>\r\n    <title><%blogsetting(name)%></title>\r\n    <link><%blogsetting(url)%></link>\r\n    <description><%blogsetting(desc)%></description>\r\n    <language>ja</language>\r\n    <generator><%version%></generator>\r\n    <copyright>&#169;</copyright>\r\n    <category>Weblog</category>\r\n    <docs>http://backend.userland.com/rss</docs>\r\n    <image>\r\n      <url><%adminurl%>nucleus2.gif</url>\r\n      <title><%blogsetting(name)%></title>\r\n      <link><%blogsetting(url)%></link>\r\n    </image>\r\n    <%blog(feeds/rss20,10)%>\r\n  </channel>\r\n</rss>');
-
 CREATE TABLE `nucleus_skin_desc` (
   `sdnumber`  int(11)     NOT NULL auto_increment,
   `sdname`    varchar(20) NOT NULL default '',
@@ -217,14 +238,8 @@ CREATE TABLE `nucleus_skin_desc` (
   `sdincmode` varchar(10) NOT NULL default 'normal',
   `sdincpref` varchar(50) NOT NULL default '',
   PRIMARY KEY  (`sdnumber`),
---  UNIQUE KEY `sdnumber` (`sdnumber`),
   UNIQUE KEY `sdname` (`sdname`)
 ) TYPE=MyISAM;
-
-INSERT INTO `nucleus_skin_desc` VALUES (2, 'feeds/atom', 'Atom 1.0 weblog syndication', 'application/atom+xml', 'normal', '');
-INSERT INTO `nucleus_skin_desc` VALUES (3, 'feeds/rss20', 'RSS 2.0 syndication of weblogs', 'text/xml', 'normal', '');
-INSERT INTO `nucleus_skin_desc` VALUES (4, 'xml/rsd', 'RSD (Really Simple Discovery) information for weblog clients', 'text/xml', 'normal', '');
-INSERT INTO `nucleus_skin_desc` VALUES (5, 'default', 'Nucleus CMS default skin', 'text/html', 'skindir', 'default/');
 
 CREATE TABLE `nucleus_team` (
   `tmember` int(11)     NOT NULL default '0',
@@ -242,33 +257,13 @@ CREATE TABLE `nucleus_template` (
   PRIMARY KEY  (`tdesc`, `tpartname`)
 ) TYPE=MyISAM;
 
-INSERT INTO `nucleus_template` VALUES (3, 'ITEM', '<item>\r\n <title><%title(xml)%></title>\r\n <link><%blogurl%>?itemid=<%itemid%></link>\r\n<description><![CDATA[<%body%><%more%>]]></description>\r\n <category><%category%></category>\r\n<comments><%blogurl%>?itemid=<%itemid%></comments>\r\n <pubDate><%date(rfc822)%></pubDate>\r\n</item>');
-INSERT INTO `nucleus_template` VALUES (3, 'EDITLINK', '<a href="<%editlink%>" onclick="<%editpopupcode%>">edit</a>');
-INSERT INTO `nucleus_template` VALUES (3, 'FORMAT_DATE', '%Y-%m-%d');
-INSERT INTO `nucleus_template` VALUES (3, 'FORMAT_TIME', '%H:%M:%S');
-INSERT INTO `nucleus_template` VALUES (4, 'ITEM', '<%date(utc)%>');
-INSERT INTO `nucleus_template` VALUES (5, 'ITEM', '<entry>\r\n <title type="html"><![CDATA[<%title%>]]></title>\r\n <link rel="alternate" type="text/html" href="<%blogurl%>?itemid=<%itemid%>" />\r\n <author>\r\n  <name><%author%></name>\r\n </author>\r\n <updated><%date(utc)%></updated>\r\n <published><%date(iso8601)%></published>\r\n <content type="html"><![CDATA[<%body%><%more%>]]></content>\r\n <id><%blogurl%>:<%blogid%>:<%itemid%></id>\r\n</entry>');
-INSERT INTO `nucleus_template` VALUES (5, 'POPUP_CODE', '<%media%>');
-INSERT INTO `nucleus_template` VALUES (5, 'IMAGE_CODE', '<%image%>');
-INSERT INTO `nucleus_template` VALUES (5, 'MEDIA_CODE', '<%media%>');
-INSERT INTO `nucleus_template` VALUES (3, 'POPUP_CODE', '<%image%>');
-INSERT INTO `nucleus_template` VALUES (3, 'MEDIA_CODE', '<%media%>');
-INSERT INTO `nucleus_template` VALUES (3, 'IMAGE_CODE', '<%media%>');
-
 CREATE TABLE `nucleus_template_desc` (
   `tdnumber` int(11)     NOT NULL auto_increment,
   `tdname`   varchar(64) NOT NULL default '',
   `tddesc`   varchar(200)         default NULL,
   PRIMARY KEY (`tdnumber`),
---  UNIQUE KEY `tdnumber` (`tdnumber`),
   UNIQUE  KEY `tdname` (`tdname`)
 ) TYPE=MyISAM;
-
-INSERT INTO `nucleus_template_desc` VALUES (4, 'feeds/atom/modified', 'Atom feeds: Inserts last modification date');
-INSERT INTO `nucleus_template_desc` VALUES (5, 'feeds/atom/entries', 'Atom feeds: Feed items');
-INSERT INTO `nucleus_template_desc` VALUES (3, 'feeds/rss20', 'Used for RSS 2.0 syndication of your blog');
-INSERT INTO `nucleus_template_desc` VALUES (8, 'default/index', 'Nucleus CMS default index template');
-INSERT INTO `nucleus_template_desc` VALUES (9, 'default/item', 'Nucleus CMS default item template');
 
 CREATE TABLE `nucleus_tickets` (
   `ticket` varchar(40) NOT NULL default '',
--- utf8/nucleus/plugins/NP_SkinFiles.php
+++ utf8/nucleus/plugins/NP_SkinFiles.php
@@ -3,148 +3,148 @@
 class NP_SkinFiles extends NucleusPlugin {
 
    /* ==========================================================================================
-	* Nucleus SkinFiles Plugin
-	*
-	* Copyright 2005-2009 by Jeff MacMichael and Niels Leenheer
-	*
-	* @version $Id$
-	* @version $NucleusJP: NP_SkinFiles.php,v 1.4 2007/02/04 06:28:46 kimitake Exp $
-	*
-	* ==========================================================================================
-	* This program is free software and open source software; you can redistribute
-	* it and/or modify it under the terms of the GNU General Public License as
-	* published by the Free Software Foundation; either version 2 of the License,
-	* or (at your option) any later version.
-	*
-	* This program is distributed in the hope that it will be useful, but WITHOUT
-	* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-	* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-	* more details.
-	*
-	* You should have received a copy of the GNU General Public License along
-	* with this program; if not, write to the Free Software Foundation, Inc.,
-	* 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
-	* http://www.gnu.org/licenses/gpl.html
-	* ==========================================================================================
-	*
-	* Changes:
-	* v0.91 ged   - added ICO, PHPx files, fixed/added some icons
-	*             - changed perms on file or folder creation or upload to 0755 from 0640
-	*             - changed 'cancel' links for delete actions to $parent dir from http_referer
-	*             - changed order of links next to files... moved 'del' over a bit.  ;)
-	* v0.92 ged   - changed order of links next to dirs
-	*               $privateskins = FALSE by default
-	* v1.0  ged   - fixed security catch so it actually quits the script
-	*               "columnated" the files & dirs display for easier viewing
-	*               Made the edit cancel link more intuitive
-	* v1.01 ged   - fixed event_QuickMenu to properly skip for non-admins
-	*               lined up columns for directories & added <tr> highlights
-	* v2.00 rakaz - Almost complete rewrite
-	* v2.01 yama  - modified form button for IE
-	* v2.02 kimitake - multilingual support, modified form button for IE
-	* v2.03 yama - CSS out source. and textarea width bug fix for IE. And some lang add.And add routine empty file delete.
-	*/
+    * Nucleus SkinFiles Plugin
+    *
+    * Copyright 2005-2009 by Jeff MacMichael and Niels Leenheer
+    *
+    * @version $Id$
+    * @version $NucleusJP: NP_SkinFiles.php,v 1.4 2007/02/04 06:28:46 kimitake Exp $
+    *
+    * ==========================================================================================
+    * This program is free software and open source software; you can redistribute
+    * it and/or modify it under the terms of the GNU General Public License as
+    * published by the Free Software Foundation; either version 2 of the License,
+    * or (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful, but WITHOUT
+    * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    * more details.
+    *
+    * You should have received a copy of the GNU General Public License along
+    * with this program; if not, write to the Free Software Foundation, Inc.,
+    * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
+    * http://www.gnu.org/licenses/gpl.html
+    * ==========================================================================================
+    *
+    * Changes:
+    * v0.91 ged   - added ICO, PHPx files, fixed/added some icons
+    *             - changed perms on file or folder creation or upload to 0755 from 0640
+    *             - changed 'cancel' links for delete actions to $parent dir from http_referer
+    *             - changed order of links next to files... moved 'del' over a bit.  ;)
+    * v0.92 ged   - changed order of links next to dirs
+    *               $privateskins = FALSE by default
+    * v1.0  ged   - fixed security catch so it actually quits the script
+    *               "columnated" the files & dirs display for easier viewing
+    *               Made the edit cancel link more intuitive
+    * v1.01 ged   - fixed event_QuickMenu to properly skip for non-admins
+    *               lined up columns for directories & added <tr> highlights
+    * v2.00 rakaz - Almost complete rewrite
+    * v2.01 yama  - modified form button for IE
+    * v2.02 kimitake - multilingual support, modified form button for IE
+    * v2.03 yama - CSS out source. and textarea width bug fix for IE. And some lang add.And add routine empty file delete.
+    */
 
 
-	function getName() {
-		return 'SkinFiles';
-	}
+    function getName() {
+        return 'SkinFiles';
+    }
 
-	function getAuthor() {
-		return 'Misc authors';
-	}
+    function getAuthor() {
+        return 'Misc authors';
+    }
 
-	function getURL() {
-		return 'http://wakka.xiffy.nl/skinfiles';
-	}
+    function getURL() {
+        return 'http://wakka.xiffy.nl/skinfiles';
+    }
 
-	function getVersion() {
-		return '2.03test';
-	}
+    function getVersion() {
+        return '2.03test';
+    }
 
-	function getDescription() {
-		return _SKINFILES_01;
-	}
+    function getDescription() {
+        return _SKINFILES_01;
+    }
 
-	function supportsFeature($what) {
-		switch($what) {
-			case 'SqlTablePrefix':
-			case 'HelpPage':
-				return 1;
-			default:
-				return 0;
-		}
-	}
+    function supportsFeature($what) {
+        switch($what) {
+            case 'SqlTablePrefix':
+            case 'HelpPage':
+                return 1;
+            default:
+                return 0;
+        }
+    }
 
-	function install() {
-		$this->createOption(
-			'generate_backup',
-			_SKINFILES_OPT_GENBACKUP,
-			'yesno',
-			'yes'
-		);
-		$this->createOption(
-			'backup_prefix',
-			_SKINFILES_OPT_BACKUPPREFIX,
-			'text',
-			'bkup_'
-		);
-	}
-	
-	function unInstall() {
-	}
+    function install() {
+        $this->createOption(
+            'generate_backup',
+            _SKINFILES_OPT_GENBACKUP,
+            'yesno',
+            'yes'
+        );
+        $this->createOption(
+            'backup_prefix',
+            _SKINFILES_OPT_BACKUPPREFIX,
+            'text',
+            'bkup_'
+        );
+    }
+    
+    function unInstall() {
+    }
 
-	function getEventList() {
-		return array(
-			'QuickMenu',
-			'AdminPrePageHead'
-		);
-	}
-	
-	function hasAdminArea() {
-		return 1;
-	}
+    function getEventList() {
+        return array(
+            'QuickMenu',
+            'AdminPrePageHead'
+        );
+    }
+    
+    function hasAdminArea() {
+        return 1;
+    }
 
-	function init() {
-		// include language file for this plugin
-		$language = preg_replace( '@\\|/@', '', getLanguageName());
-		if (file_exists($this->getDirectory().$language.'.php'))
-			include_once($this->getDirectory().$language.'.php');
-		else
-			include_once($this->getDirectory().'english.php');
-	}
-	
-	function event_QuickMenu(&$data) {
-		global $member;
+    function init() {
+        // include language file for this plugin
+        $language = preg_replace( '@\\|/@', '', getLanguageName());
+        if (file_exists($this->getDirectory().$language.'.php'))
+            include_once($this->getDirectory().$language.'.php');
+        else
+            include_once($this->getDirectory().'english.php');
+    }
+    
+    function event_QuickMenu(&$data) {
+        global $member;
 
-		// only show to admins
-		if (!($member->isLoggedIn() && $member->isAdmin())) return;
+        // only show to admins
+        if (!($member->isLoggedIn() && $member->isAdmin())) return;
 
-		array_push(
-			$data['options'], 
-			array(
-				'title'   => _SKINFILES_TITLE,
-				'url'     => $this->getAdminURL(),
-				'tooltip' => _SKINFILES_TOOLTIP
-			)
-		);
-	}
+        array_push(
+            $data['options'], 
+            array(
+                'title'   => _SKINFILES_TITLE,
+                'url'     => $this->getAdminURL(),
+                'tooltip' => _SKINFILES_TOOLTIP
+            )
+        );
+    }
 
 
 // start add yama.kyms
-	function event_AdminPrePageHead(&$data) {
-		global $CONF;
-		$path = $CONF['PluginURL'];
-		if ($data['action'] != 'plugin_SkinFiles') return;
-		
-			$data['extrahead'] .= <<< EOS
+    function event_AdminPrePageHead($data) {
+        global $CONF;
+        $path = $CONF['PluginURL'];
+        if ($data['action'] != 'plugin_SkinFiles') return;
+        
+            $data['extrahead'] .= <<< EOS
 
 <link rel="stylesheet" type="text/css" href="{$path}skinfiles/style.css" />
 
 
 EOS;
-		}
-		
+        }
+        
 
 
 // end add yama.kyms
