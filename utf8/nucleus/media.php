332024406cb5eb8f9a57624ae5cad281973d13a3
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2009-07-19 05:25:41 +0000

    FIX: super-admin disallowed from uploading files if not on any teamlists.
    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/nucleus-jp/trunk@1042 1ca29b6e-896d-4ea0-84a5-967f57386b96

--- utf8/nucleus/media.php
+++ utf8/nucleus/media.php
@@ -48,7 +48,7 @@ if (!$member->isLoggedIn()) {
 // check if member is on at least one teamlist
 $query = 'SELECT * FROM ' . sql_table('team'). ' WHERE tmember=' . $member->getID();
 $teams = sql_query($query);
-if (sql_num_rows($teams) == 0)
+if (sql_num_rows($teams) == 0 && !$member->isAdmin())
 	media_doError(_ERROR_DISALLOWEDUPLOAD);
 
 // get action
