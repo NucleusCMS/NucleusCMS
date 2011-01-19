cde8bea5a3a91806dc000e01cd2164d6d8bdf945
sakamocchi <sakamocchi@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2011-01-19 08:54:44 +0000

    Modify some description for refering versions and missed lines for English.
    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/nucleus-jp/trunk@1098 1ca29b6e-896d-4ea0-84a5-967f57386b96

--- utf8/nucleus/language/japanese-euc.php
+++ utf8/nucleus/language/japanese-euc.php
@@ -28,12 +28,18 @@
  */
 
 /********************************************
+ *        Start New for 3.62                *
+ ********************************************/
+define('_SETTINGS_ADMINCSS',		'管理領域のスタイルシート');
+
+
+/********************************************
  *        Start New for 3.50                *
  ********************************************/
 define('_PLUGS_TITLE_GETPLUGINS',		'プラグインを入手…');
 define('_ARCHIVETYPE_YEAR', '年');
-define('_ADMIN_SYSTEMOVERVIEW_LATESTVERSION_TITLE',		'新しいバージョンが入手可能です');
-define('_ADMIN_SYSTEMOVERVIEW_LATESTVERSION_TEXT',		'アップグレードが入手可能です： v');
+define('_ADMIN_SYSTEMOVERVIEW_LATESTVERSION_TITLE',		'新しいバージョンが入手可能かもしれません');
+define('_ADMIN_SYSTEMOVERVIEW_LATESTVERSION_TEXT',		'アップグレードが入手可能かもしれません： v');
 define('_MANAGER_PLUGINSQLAPI_NOTSUPPORT', "プラグイン %s を読み込めませんでした。(このプラグインはSqlAPIをサポートしていません。データベースの接続方法を見直してください)");
 
 
