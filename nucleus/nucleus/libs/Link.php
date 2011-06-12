544dec98331b73d279f9b59aec826b3f7b5bc90c
kaigreve <kaigreve@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2011-06-12 21:28:19 +0000

    ADD: a new class Link with a collection of all functions which create links
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@1518 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

---
+++ nucleus/nucleus/libs/Link.php
@@ -0,0 +1,201 @@
+<?php 
+/*
+ * Nucleus: PHP/MySQL Weblog CMS (http://nucleuscms.org/)
+ * Copyright (C) 2002-2011 The Nucleus Group
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * (see nucleus/documentation/index.html#license for more info)
+ */
+/**
+ * This class is a collections of functions that produce links
+ * 
+ * All functions in this clss should only be called statically,
+ * for example: Link::createItemLink(...)
+ * 
+ * @license http://nucleuscms.org/license.txt GNU General Public License
+ * @copyright Copyright (C) 2002-2011 The Nucleus Group
+ * @version $Id$
+ */
+class Link {
+
+	function createItemLink($itemid, $extra = '') {
+	    return Link::createLink('item', array('itemid' => $itemid, 'extra' => $extra) );
+	}
+
+	function createMemberLink($memberid, $extra = '') {
+	    return Link::createLink('member', array('memberid' => $memberid, 'extra' => $extra) );
+	}
+
+	function createCategoryLink($catid, $extra = '') {
+	    return Link::createLink('category', array('catid' => $catid, 'extra' => $extra) );
+	}
+
+	function createArchiveLink($blogid, $archive, $extra = '') {
+	    return Link::createLink('archive', array('blogid' => $blogid, 'archive' => $archive, 'extra' => $extra) );
+	}
+
+	function createArchiveListLink($blogid = '', $extra = '') {
+	    return Link::createLink('archivelist', array('blogid' => $blogid, 'extra' => $extra) );
+	}
+
+	function createBlogidLink($blogid, $params = '') {
+	    return Link::createLink('blog', array('blogid' => $blogid, 'extra' => $params) );
+	}
+
+	function createLink($type, $params) {
+	    global $manager, $CONF;
+	
+	    $generatedURL = '';
+	    $usePathInfo = ($CONF['URLMode'] == 'pathinfo');
+	
+	    // ask plugins first
+	    $created = false;
+	
+	    if ($usePathInfo) {
+	        $manager->notify(
+	            'GenerateURL',
+	            array(
+	                'type' => $type,
+	                'params' => $params,
+	                'completed' => &$created,
+	                'url' => &$url
+	            )
+	        );
+	    }
+	
+	    // if a plugin created the URL, return it
+	    if ($created) {
+	        return $url;
+	    }
+	
+	    // default implementation
+	    switch ($type) {
+	        case 'item':
+	            if ($usePathInfo) {
+	                $url = $CONF['ItemURL'] . '/' . $CONF['ItemKey'] . '/' . $params['itemid'];
+	            } else {
+	                $url = $CONF['ItemURL'] . '?itemid=' . $params['itemid'];
+	            }
+	            break;
+	
+	        case 'member':
+	            if ($usePathInfo) {
+	                $url = $CONF['MemberURL'] . '/' . $CONF['MemberKey'] . '/' . $params['memberid'];
+	            } else {
+	                $url = $CONF['MemberURL'] . '?memberid=' . $params['memberid'];
+	            }
+	            break;
+	
+	        case 'category':
+	            if ($usePathInfo) {
+	                $url = $CONF['CategoryURL'] . '/' . $CONF['CategoryKey'] . '/' . $params['catid'];
+	            } else {
+	                $url = $CONF['CategoryURL'] . '?catid=' . $params['catid'];
+	            }
+	            break;
+	
+	        case 'archivelist':
+	            if (!$params['blogid']) {
+	                $params['blogid'] = $CONF['DefaultBlog'];
+	            }
+	
+	            if ($usePathInfo) {
+	                $url = $CONF['ArchiveListURL'] . '/' . $CONF['ArchivesKey'] . '/' . $params['blogid'];
+	            } else {
+	                $url = $CONF['ArchiveListURL'] . '?archivelist=' . $params['blogid'];
+	            }
+	            break;
+	
+	        case 'archive':
+	            if ($usePathInfo) {
+	                $url = $CONF['ArchiveURL'] . '/' . $CONF['ArchiveKey'] . '/'.$params['blogid'].'/' . $params['archive'];
+	            } else {
+	                $url = $CONF['ArchiveURL'] . '?blogid='.$params['blogid'].'&amp;archive=' . $params['archive'];
+	            }
+	            break;
+	
+	        case 'blog':
+	            if ($usePathInfo) {
+	                $url = $CONF['BlogURL'] . '/' . $CONF['BlogKey'] . '/' . $params['blogid'];
+	            } else {
+	                $url = $CONF['BlogURL'] . '?blogid=' . $params['blogid'];
+	            }
+	            break;
+	    }
+	
+	    return Link::addLinkParams($url, (isset($params['extra'])? $params['extra'] : null));
+	}
+
+	function addLinkParams($link, $params) {
+	    global $CONF;
+	
+	    if (is_array($params) ) {
+	
+	        if ($CONF['URLMode'] == 'pathinfo') {
+	
+	            foreach ($params as $param => $value) {
+	                // change in 3.63 to fix problem where URL generated with extra params mike look like category/4/blogid/1
+					// but they should use the URL keys like this: category/4/blog/1
+					// if user wants old urls back, set $CONF['NoURLKeysInExtraParams'] = 1; in config.php
+	                if (isset($CONF['NoURLKeysInExtraParams']) && $CONF['NoURLKeysInExtraParams'] == 1) 
+					{
+						$link .= '/' . $param . '/' . urlencode($value);
+					} else {
+						switch ($param) {
+							case 'itemid':
+								$link .= '/' . $CONF['ItemKey'] . '/' . urlencode($value);
+							break;
+							case 'memberid':
+								$link .= '/' . $CONF['MemberKey'] . '/' . urlencode($value);
+							break;
+							case 'catid':
+								$link .= '/' . $CONF['CategoryKey'] . '/' . urlencode($value);
+							break;
+							case 'archivelist':
+								$link .= '/' . $CONF['ArchivesKey'] . '/' . urlencode($value);
+							break;
+							case 'archive':
+								$link .= '/' . $CONF['ArchiveKey'] . '/' . urlencode($value);
+							break;
+							case 'blogid':
+								$link .= '/' . $CONF['BlogKey'] . '/' . urlencode($value);
+							break;
+							default:
+								$link .= '/' . $param . '/' . urlencode($value);
+							break;
+						}
+					}
+	            }
+	
+	        } else {
+	
+	            foreach ($params as $param => $value) {
+	                $link .= '&amp;' . $param . '=' . urlencode($value);
+	            }
+	
+	        }
+	    }
+	
+	    return $link;
+	}
+
+	function createBlogLink($url, $params) {
+	    global $CONF;
+	    if ($CONF['URLMode'] == 'normal') {
+	        if (strpos($url, '?') === FALSE && is_array($params)) {
+	            $fParam = reset($params);
+	            $fKey   = key($params);
+	            array_shift($params);
+	            $url .= '?' . $fKey . '=' . $fParam;
+	        }
+	    } elseif ($CONF['URLMode'] == 'pathinfo' && substr($url, -1) == '/') {
+	        $url = substr($url, 0, -1);
+	    }
+	    return addLinkParams($url, $params);
+	}
+
+}
+?>
--- nucleus/nucleus/libs/globalfunctions.php
+++ nucleus/nucleus/libs/globalfunctions.php
@@ -353,7 +353,7 @@ include($DIR_LIBS . 'BAN.php');
 include($DIR_LIBS . 'PAGEFACTORY.php');
 include($DIR_LIBS . 'SEARCH.php');
 include($DIR_LIBS . 'entity.php');
-
+include($DIR_LIBS . 'Link.php');
 
 // set lastVisit cookie (if allowed)
 if (!headers_sent() ) {
@@ -1461,182 +1461,41 @@ function checkPlugin($plug) {
 
 /**
   * Centralisation of the functions that generate links
+  * Deprecated since 3.70:
+  * Please use Link::FunctionName(...) instead
   */
 function createItemLink($itemid, $extra = '') {
-    return createLink('item', array('itemid' => $itemid, 'extra' => $extra) );
+	return Link::createItemLink($itemid, $extra);
 }
 
 function createMemberLink($memberid, $extra = '') {
-    return createLink('member', array('memberid' => $memberid, 'extra' => $extra) );
+	return Link::createMemberLink($memberid, $extra);
 }
 
 function createCategoryLink($catid, $extra = '') {
-    return createLink('category', array('catid' => $catid, 'extra' => $extra) );
+	return Link::createCategoryLink($catid, $extra);
 }
 
 function createArchiveListLink($blogid = '', $extra = '') {
-    return createLink('archivelist', array('blogid' => $blogid, 'extra' => $extra) );
+	return Link::createArchiveListLink($blogid, $extra);
 }
 
 function createArchiveLink($blogid, $archive, $extra = '') {
-    return createLink('archive', array('blogid' => $blogid, 'archive' => $archive, 'extra' => $extra) );
+	return Link::createArchiveLink($blogid, $archive, $extra);
 }
 
 function createBlogidLink($blogid, $params = '') {
-    return createLink('blog', array('blogid' => $blogid, 'extra' => $params) );
+	return Link::createBlogidLink($blogid, $params = '');
 }
 
 function createLink($type, $params) {
-    global $manager, $CONF;
-
-    $generatedURL = '';
-    $usePathInfo = ($CONF['URLMode'] == 'pathinfo');
-
-    // ask plugins first
-    $created = false;
-
-    if ($usePathInfo) {
-        $manager->notify(
-            'GenerateURL',
-            array(
-                'type' => $type,
-                'params' => $params,
-                'completed' => &$created,
-                'url' => &$url
-            )
-        );
-    }
-
-    // if a plugin created the URL, return it
-    if ($created) {
-        return $url;
-    }
-
-    // default implementation
-    switch ($type) {
-        case 'item':
-            if ($usePathInfo) {
-                $url = $CONF['ItemURL'] . '/' . $CONF['ItemKey'] . '/' . $params['itemid'];
-            } else {
-                $url = $CONF['ItemURL'] . '?itemid=' . $params['itemid'];
-            }
-            break;
-
-        case 'member':
-            if ($usePathInfo) {
-                $url = $CONF['MemberURL'] . '/' . $CONF['MemberKey'] . '/' . $params['memberid'];
-            } else {
-                $url = $CONF['MemberURL'] . '?memberid=' . $params['memberid'];
-            }
-            break;
-
-        case 'category':
-            if ($usePathInfo) {
-                $url = $CONF['CategoryURL'] . '/' . $CONF['CategoryKey'] . '/' . $params['catid'];
-            } else {
-                $url = $CONF['CategoryURL'] . '?catid=' . $params['catid'];
-            }
-            break;
-
-        case 'archivelist':
-            if (!$params['blogid']) {
-                $params['blogid'] = $CONF['DefaultBlog'];
-            }
-
-            if ($usePathInfo) {
-                $url = $CONF['ArchiveListURL'] . '/' . $CONF['ArchivesKey'] . '/' . $params['blogid'];
-            } else {
-                $url = $CONF['ArchiveListURL'] . '?archivelist=' . $params['blogid'];
-            }
-            break;
-
-        case 'archive':
-            if ($usePathInfo) {
-                $url = $CONF['ArchiveURL'] . '/' . $CONF['ArchiveKey'] . '/'.$params['blogid'].'/' . $params['archive'];
-            } else {
-                $url = $CONF['ArchiveURL'] . '?blogid='.$params['blogid'].'&amp;archive=' . $params['archive'];
-            }
-            break;
-
-        case 'blog':
-            if ($usePathInfo) {
-                $url = $CONF['BlogURL'] . '/' . $CONF['BlogKey'] . '/' . $params['blogid'];
-            } else {
-                $url = $CONF['BlogURL'] . '?blogid=' . $params['blogid'];
-            }
-            break;
-    }
-
-    return addLinkParams($url, (isset($params['extra'])? $params['extra'] : null));
+	return Link::createLink($type, $params);
 }
 
 function createBlogLink($url, $params) {
-    global $CONF;
-    if ($CONF['URLMode'] == 'normal') {
-        if (strpos($url, '?') === FALSE && is_array($params)) {
-            $fParam = reset($params);
-            $fKey   = key($params);
-            array_shift($params);
-            $url .= '?' . $fKey . '=' . $fParam;
-        }
-    } elseif ($CONF['URLMode'] == 'pathinfo' && substr($url, -1) == '/') {
-        $url = substr($url, 0, -1);
-    }
-    return addLinkParams($url, $params);
+   return Link::createBlogLink($url, $params);
 }
 
-function addLinkParams($link, $params) {
-    global $CONF;
-
-    if (is_array($params) ) {
-
-        if ($CONF['URLMode'] == 'pathinfo') {
-
-            foreach ($params as $param => $value) {
-                // change in 3.63 to fix problem where URL generated with extra params mike look like category/4/blogid/1
-				// but they should use the URL keys like this: category/4/blog/1
-				// if user wants old urls back, set $CONF['NoURLKeysInExtraParams'] = 1; in config.php
-                if (isset($CONF['NoURLKeysInExtraParams']) && $CONF['NoURLKeysInExtraParams'] == 1) 
-				{
-					$link .= '/' . $param . '/' . urlencode($value);
-				} else {
-					switch ($param) {
-						case 'itemid':
-							$link .= '/' . $CONF['ItemKey'] . '/' . urlencode($value);
-						break;
-						case 'memberid':
-							$link .= '/' . $CONF['MemberKey'] . '/' . urlencode($value);
-						break;
-						case 'catid':
-							$link .= '/' . $CONF['CategoryKey'] . '/' . urlencode($value);
-						break;
-						case 'archivelist':
-							$link .= '/' . $CONF['ArchivesKey'] . '/' . urlencode($value);
-						break;
-						case 'archive':
-							$link .= '/' . $CONF['ArchiveKey'] . '/' . urlencode($value);
-						break;
-						case 'blogid':
-							$link .= '/' . $CONF['BlogKey'] . '/' . urlencode($value);
-						break;
-						default:
-							$link .= '/' . $param . '/' . urlencode($value);
-						break;
-					}
-				}
-            }
-
-        } else {
-
-            foreach ($params as $param => $value) {
-                $link .= '&amp;' . $param . '=' . urlencode($value);
-            }
-
-        }
-    }
-
-    return $link;
-}
 
 /**
  * @param $querystr
