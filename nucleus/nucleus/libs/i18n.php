31482000b8cc33c3eff5403c27d5ac29625394d6
sakamocchi <sakamocchi@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2011-07-09 08:56:25 +0000

    Fix: revision 1553 has a problem. $delimiter should be keep for self::strpos but overwritten as preg expression...
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@1554 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/nucleus/libs/i18n.php
+++ nucleus/nucleus/libs/i18n.php
@@ -207,8 +207,8 @@ class i18n {
 	public static function explode($delimiter, $target, $limit=0)
 	{
 		$array = array();
-		$delimiter = '#' . preg_quote($delimiter, '#') . '#';
-		if ( preg_match($delimiter, $target) === 0 )
+		$preg_delimiter = '#' . preg_quote($delimiter, '#') . '#';
+		if ( preg_match($preg_delimiter, $target) === 0 )
 		{
 			return (array) $target;
 		}
