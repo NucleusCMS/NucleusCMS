0c40c2cf002eac875c67d9276b26931e7b9e8466
gregorlove <gregorlove@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2011-07-17 04:47:26 +0000

    Begin code cleanup / documentation.
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@1569 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/nucleus/libs/mysql.php
+++ nucleus/nucleus/libs/mysql.php
@@ -15,112 +15,197 @@
  * @copyright Copyright (C) 2002-2007 The Nucleus Group
  * @version $Id$
  */
- 
-/*
- * if no mysql_* functions exist, define wrappers
- */
- 
-$MYSQL_CONN = 0;
 
-if (!function_exists('mysql_query'))
-{
-	if (!function_exists('mysqli_query') && function_exists('startUpError'))
-	{
-		startUpError(_NO_SUITABLE_MYSQL_LIBRARY);
-	}
-	
-	function mysql_query($query) 
-	{
-		global $MYSQL_CONN;
-		return mysqli_query($MYSQL_CONN, $query); 
-	}
-	
-	function mysql_fetch_object($res) 
-	{ 
-		return mysqli_fetch_object($res);
-	}
-	
-	function mysql_fetch_array($res) 
-	{ 
-		return mysqli_fetch_array($res);
-	}	
-	
-	function mysql_fetch_assoc($res) 
-	{ 
-		return mysqli_fetch_assoc($res);
-	}	
+	$MYSQL_CONN = 0;
 
-	function mysql_fetch_row($res) 
-	{ 
-		return mysqli_fetch_row($res);
-	}	
+	// begin if: no mysql_* functions exist, define wrappers
+	if ( !function_exists('mysql_query') )
+	{
 
-	function mysql_num_rows($res)
-	{
-		return mysqli_num_rows($res);
-	}
-	
-	function mysql_num_fields($res)
-	{
-		return mysqli_num_fields($res);
-	}
-	
-	function mysql_free_result($res)
-	{
-		return mysqli_free_result($res);
-	}
-	
-	function mysql_result($res, $row, $col) 
-	{ 
-		if (($row != 0) || ($col != 0)) {
-			trigger_error('not implemented', E_USER_ERROR);
+		// begin if: mysqli_query() function does not exist; no suitable MySQL library
+		if ( !function_exists('mysqli_query') && function_exists('startUpError') )
+		{
+			startUpError(_NO_SUITABLE_MYSQL_LIBRARY);
+		} // end if
+
+
+		/**
+		 * This function is a wrapper for mysqli_query()
+		 * @param string $query
+		 * @return resource|bool
+		 */
+		function mysql_query($query)
+		{
+			global $MYSQL_CONN;
+			return mysqli_query($MYSQL_CONN, $query);
 		}
-		
-		$row = mysqli_fetch_row($res);
-		return $row[$col];
-	}	
-	
-	function mysql_connect($host, $username, $pwd)
-	{
-		return mysqli_connect($host, $username, $pwd);
+
+
+		/**
+		 * This function is a wrapper for mysqli_fetch_object
+		 * @param resource $result
+		 * @return object|null
+		 */
+		function mysql_fetch_object($result)
+		{
+			return mysqli_fetch_object($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_fetch_array
+		 * @param resource $result
+		 * @return array|null
+		 */
+		function mysql_fetch_array($result)
+		{
+			return mysqli_fetch_array($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_fetch_assoc
+		 * @param resource $result
+		 * @return array|null
+		 */
+		function mysql_fetch_assoc($result)
+		{
+			return mysqli_fetch_assoc($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_fetch_row
+		 * @param resource $result
+		 * @return array|null
+		 */
+		function mysql_fetch_row($result)
+		{
+			return mysqli_fetch_row($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_num_rows
+		 * @param resource $result
+		 * @return int|string
+		 */
+		function mysql_num_rows($result)
+		{
+			return mysqli_num_rows($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_num_fields
+		 * @param resource $result
+		 * @return int
+		 */
+		function mysql_num_fields($result)
+		{
+			return mysqli_num_fields($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_free_result
+		 * @param resource $result
+		 * @return null
+		 */
+		function mysql_free_result($result)
+		{
+			return mysqli_free_result($result);
+		}
+
+
+		/**
+		 * This function is a wrapper for mysqli_free_result
+		 * @param resource $result
+		 * @return string
+		 */
+		function mysql_result($result, $row, $column)
+		{
+
+			// begin if: a row or column other than the initial one is selected; not implemented
+			if ( ($row != 0) || ($column != 0) )
+			{
+				trigger_error('not implemented', E_USER_ERROR);
+			} // end if
+
+			$row = mysqli_fetch_row($result);
+			return $row[$column];
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_connect($host, $username, $pwd)
+		{
+			return mysqli_connect($host, $username, $pwd);
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_error()
+		{
+			global $MYSQL_CONN;
+			return mysqli_error($MYSQL_CONN);
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_select_db($db)
+		{
+			global $MYSQL_CONN;
+			return mysqli_select_db($MYSQL_CONN, $db);
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_close()
+		{
+			global $MYSQL_CONN;
+			return mysqli_close($MYSQL_CONN);
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_insert_id()
+		{
+			global $MYSQL_CONN;
+			return mysqli_insert_id($MYSQL_CONN);
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_affected_rows()
+		{
+			global $MYSQL_CONN;
+			return mysqli_affected_rows($MYSQL_CONN);
+		}
+
+
+		/**
+		 *
+		 */
+		function mysql_real_escape_string($val)
+		{
+			global $MYSQL_CONN;
+			return mysqli_real_escape_string($MYSQL_CONN,$val);
+		}
+
 	}
-	
-	function mysql_error()
-	{
-		global $MYSQL_CONN;
-		return mysqli_error($MYSQL_CONN);
-	}
-	
-	function mysql_select_db($db)
-	{
-		global $MYSQL_CONN;
-		return mysqli_select_db($MYSQL_CONN, $db);
-	}
-	
-	function mysql_close()
-	{
-		global $MYSQL_CONN;
-		return mysqli_close($MYSQL_CONN);
-	}
-	
-	function mysql_insert_id()
-	{
-		global $MYSQL_CONN;
-		return mysqli_insert_id($MYSQL_CONN);
-	}
-	
-	function mysql_affected_rows()
-	{
-		global $MYSQL_CONN;
-		return mysqli_affected_rows($MYSQL_CONN);
-	}
-	
-	function mysql_real_escape_string($val)
-	{
-		global $MYSQL_CONN;
-		return mysqli_real_escape_string($MYSQL_CONN,$val);
-	}
-}
 
 
 
