ccaf637fb8d06d6661039c7b8203b924cccdb11f
sakamocchi <sakamocchi@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2012-01-29 08:07:27 +0000

    Code cleanup for ./nucleus/index.php
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@1638 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/nucleus/index.php
+++ nucleus/nucleus/index.php
@@ -14,63 +14,69 @@
  * @copyright Copyright (C) 2002-2009 The Nucleus Group
  * @version $Id$
  */
-	// we are using admin stuff:
-	$CONF = array();
-	$CONF['UsingAdminArea'] = 1;
 
-	// include the admin code
-	require_once('../config.php');
+// we are using admin stuff:
+$CONF = array();
+$CONF['UsingAdminArea'] = 1;
 
-	if ($CONF['alertOnSecurityRisk'] == 1)
+// include the admin code
+require_once('../config.php');
+
+if ($CONF['alertOnSecurityRisk'] == 1)
+{
+	// check if files exist and generate an error if so
+	$aFiles = array(
+		'../install.sql' => _ERRORS_INSTALLSQL,
+		'../install.php' => _ERRORS_INSTALLPHP,
+		'upgrades' => _ERRORS_UPGRADESDIR,
+		'convert' => _ERRORS_CONVERTDIR
+	);
+	$aFound = array();
+	foreach($aFiles as $fileName => $fileDesc)
 	{
-		// check if files exist and generate an error if so
-		$aFiles = array(
-			'../install.sql' => _ERRORS_INSTALLSQL,
-			'../install.php' => _ERRORS_INSTALLPHP,
-			'upgrades' => _ERRORS_UPGRADESDIR,
-			'convert' => _ERRORS_CONVERTDIR
+		if (@file_exists($fileName))
+			array_push($aFound, $fileDesc);
+	}
+	if (@is_writable('../config.php')) {
+		array_push($aFound, _ERRORS_CONFIGPHP);
+	}
+	if (sizeof($aFound) > 0)
+	{
+		startUpError(
+			_ERRORS_STARTUPERROR1. implode($aFound, '</li><li>')._ERRORS_STARTUPERROR2,
+			_ERRORS_STARTUPERROR3
 		);
-		$aFound = array();
-		foreach($aFiles as $fileName => $fileDesc)
-		{
-			if (@file_exists($fileName))
-				array_push($aFound, $fileDesc);
-		}
-		if (@is_writable('../config.php')) {
-			array_push($aFound, _ERRORS_CONFIGPHP);
-		}
-		if (sizeof($aFound) > 0)
-		{
-			startUpError(
-				_ERRORS_STARTUPERROR1. implode($aFound, '</li><li>')._ERRORS_STARTUPERROR2,
-				_ERRORS_STARTUPERROR3
-			);
-		}
 	}
+}
 
-	$bNeedsLogin = false;
-	$bIsActivation = in_array($action, array('activate', 'activatesetpwd'));
+$bNeedsLogin = false;
+$bIsActivation = in_array($action, array('activate', 'activatesetpwd'));
 
-	if ($action == 'logout')
-		$bNeedsLogin = true;
+if ( $action == 'logout' )
+{
+	$bNeedsLogin = true;
+}
 
-	if (!$member->isLoggedIn() && !$bIsActivation)
-		$bNeedsLogin = true;
+if ( !$member->isLoggedIn() && !$bIsActivation )
+{
+	$bNeedsLogin = true;
+}
 
-	// show error if member cannot login to admin
-	if ($member->isLoggedIn() && !$member->canLogin() && !$bIsActivation) {
-		$error = _ERROR_LOGINDISALLOWED;
-		$bNeedsLogin = true;
-	}
+// show error if member cannot login to admin
+if ( $member->isLoggedIn() && !$member->canLogin() && !$bIsActivation )
+{
+	$error = _ERROR_LOGINDISALLOWED;
+	$bNeedsLogin = true;
+}
 
-	if ($bNeedsLogin)
-	{
-		setOldAction($action);	// see ADMIN::login() (sets old action in POST vars)
-		$action = 'showlogin';
-	}
+if ( $bNeedsLogin )
+{
+	// see ADMIN::login() (sets old action in POST vars)
+	setOldAction($action);
+	$action = 'showlogin';
+}
 
-	sendContentType('text/html', 'admin-' . $action);
+sendContentType('text/html', 'admin-' . $action);
 
-	$admin = new ADMIN();
-	$admin->action($action);
-?>
+$admin = new ADMIN();
+$admin->action($action);
