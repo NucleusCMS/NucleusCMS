0c5aaca5b8028bd1ce8734154bdb12d7d69e92ea
sakamocchi <sakamocchi@dbb57b54-ea0f-0410-8215-9b63e7fa3c7b>
2012-04-07 05:07:08 +0000

    code clean-up for BaseActions class
    Adding access modifications and comments
    
    git-svn-id: https://nucleuscms.svn.sourceforge.net/svnroot/nucleuscms/trunk@1729 dbb57b54-ea0f-0410-8215-9b63e7fa3c7b

--- nucleus/nucleus/libs/BaseActions.php
+++ nucleus/nucleus/libs/BaseActions.php
@@ -23,201 +23,279 @@
 class BaseActions
 {
 	// depth level for includes (max. level is 3)
-	var $level;
-
+	public $level;
+	
 	// array of evaluated conditions (true/false). The element at the end is the one for the most nested
 	// if block.
-	var $if_conditions;
-
+	public $if_conditions;
+	
 	// in the "elseif" / "elseifnot" sequences, if one of the conditions become "true" remained conditions should not
 	// be tested. this variable (actually a stack) holds this information.
-	var $if_execute;
-
+	public $if_execute;
+	
 	// at all times, can be evaluated to either true if the current block needs to be displayed. This
 	// variable is used to decide to skip skinvars in parts that will never be outputted.
-	var $if_currentlevel;
-
+	public $if_currentlevel;
+	
 	// contains a search string with keywords that need to be highlighted. These get parsed into $aHighlight
-	var $strHighlight;
-
+	public $strHighlight;
+	
 	// array of keywords that need to be highlighted in search results (see the highlight()
 	// and parseHighlight() methods)
-	var $aHighlight;
-
+	public $aHighlight;
+	
 	// reference to the parser object that is using this object as actions-handler
-	var $parser;
-
+	public $parser;
+	
 	/**
+	 * BaseActions::BaseActions()
 	 *  Constructor for a new BaseAction object
 	 */
-	function BaseActions() {
+	protected function BaseActions()
+	{
 		$this->level = 0;
-
+		
 		// if nesting level
 		$this->if_conditions = array(); // array on which condition values are pushed/popped
 		$this->if_execute = array(); 	// array on which condition values are pushed/popped
 		$this->if_currentlevel = 1;		// 1 = current level is displayed; 0 = current level not displayed
-
+		
 		// highlights
 		$this->strHighlight = '';			// full highlight
 		$this->aHighlight = array();		// parsed highlight
-
+		return;
 	}
-
+	
 	/**
+	 * BaseActions::parse_include()
 	 * include file (no parsing of php)
 	 * 
 	 * ToDo: function returns nothing and refering to the cross reference it
 	 *       isn't called from anywhere   
 	 * 
-	 * @param $filename
+	 * @param	string	$filename	filename to be included
+	 * @return	void
 	 */
-	function parse_include($filename) {
+	public function parse_include($filename)
+	{
 		@readfile($this->getIncludeFileName($filename));
+		return;
 	}
-
-	/**
-	 * php-include file
-	 * 
-	 * @param $filename
-	 */
-	function parse_phpinclude($filename) {
-		includephp($this->getIncludeFileName($filename));
-	}
-
 	
 	/**
+	 * BaseActions::parse_phpinclude()
+	 * php-include file
+	 * 
+	 * @param	string	$filename	filename to be included
+	 * @return	void
+	 */
+	public function parse_phpinclude($filename)
+	{
+		includephp($this->getIncludeFileName($filename));
+		return;
+	}
+	
+	
+	/**
+	 * BaseActions::parse_parsedinclude()
 	 * parsed include
 	 * 
-	 * @param $filename
+	 * @param	string	$filename	filename to be included
+	 * @return	void
 	 */
-	function parse_parsedinclude($filename) {
+	public function parse_parsedinclude($filename)
+	{
 		// check current level
-		if ($this->level > 3) return;	// max. depth reached (avoid endless loop)
+		if ( $this->level > 3 )
+		{
+			// max. depth reached (avoid endless loop)
+			return;
+		}
+		
 		$file = $this->getIncludeFileName($filename);
-		if (!file_exists($file)) return;
-        $contents = file_get_contents($file);
-        if (empty($contents)) return;
+		
+		if ( !file_exists($file) )
+		{
+			return;
+		}
+		
+		$contents = file_get_contents($file);
+		
+		if ( empty($contents) )
+		{
+			return;
+		}
 		
 		$this->level = $this->level + 1;
 		// parse file contents
 		$this->parser->parse($contents);
-
+		
 		$this->level = $this->level - 1;
+		return;
 	}
-
+	
 	/**
+	 * BaseActions::getIncludeFileName()
 	 * Returns the correct location of the file to be included, according to
 	 * parser properties
 	 *
 	 * IF IncludeMode = 'skindir' => use skindir
 	 * 
-	 * @param $filename
+	 * @param	string	$filename	name of file to be inclluded
+	 * @return	string	name of file with relative path
 	 */
-	function getIncludeFileName($filename) {
+	public function getIncludeFileName($filename)
+	{
 		// leave absolute filenames and http urls as they are
 		if (
 				(i18n::substr($filename,0,1) == '/')
 			||	(i18n::substr($filename,0,7) == 'http://')
 			||	(i18n::substr($filename,0,6) == 'ftp://')
 			)
+		{
 			return $filename;
-
+		}
+		
 		$filename = Parser::getProperty('IncludePrefix') . $filename;
-		if (Parser::getProperty('IncludeMode') == 'skindir') {
+		
+		if ( Parser::getProperty('IncludeMode') == 'skindir' )
+		{
 			global $DIR_SKINS;
 			return $DIR_SKINS . $filename;
-		} else {
-			return $filename;
 		}
-	}
-
-	/**
-	 * Inserts an url relative to the skindir (useful when doing import/export)
-	 *
-	 * e.g. <skinfile(default/myfile.sth)>
-	 */
-	function parse_skinfile($filename) {
-		global $CONF;
-
-		echo $CONF['SkinsURL'] . Parser::getProperty('IncludePrefix') . $filename;
-	}
-
-	/**
-	 * Sets a property for the parser
-	 */
-	function parse_set($property, $value) {
-		Parser::setProperty($property, $value);
-	}
-
-	/**
-	 * Helper function: add if condition
-	 */
-	function _addIfCondition($condition) {
-
-		array_push($this->if_conditions,$condition);
-
-		$this->_updateTopIfCondition();
-
-		ob_start();
-	}
-
-	/**
-	 * Helper function: update the Top of the If Conditions Array
-	 */
-	function _updateTopIfCondition() {
-		if (sizeof($this->if_conditions) == 0) {
-			$this->if_currentlevel = 1;
-		}
-		else {
-			$this->if_currentlevel = $this->if_conditions[sizeof($this->if_conditions) - 1];
-		}
-	}
-
-	/**
-	 * Helper function for elseif / elseifnot
-	 */
-	function _addIfExecute() {
-		array_push($this->if_execute, 0);
-	}
-
-	/**
-	 * Helper function for elseif / elseifnot
-	 * @param string condition to be fullfilled
-	 */
-	function _updateIfExecute($condition) {
-		$index = sizeof($this->if_execute) - 1;
-		$this->if_execute[$index] = $this->if_execute[$index] || $condition;
-	}
-
-	/**
-	 * returns the currently top if condition
-	 */
-	function _getTopIfCondition() {
-		return $this->if_currentlevel;
-	}
-
-	/**
-	 * Sets the search terms to be highlighted
-	 *
-	 * @param $highlight
-	 *		A series of search terms
-	 */
-	function setHighlight($highlight) {
-		$this->strHighlight = $highlight;
-		if ($highlight) {
-			$this->aHighlight = parseHighlight($highlight);
-		}
+		return $filename;
 	}
 	
 	/**
+	 * BaseActions::parse_skinfile()
+	 * Inserts an url relative to the skindir (useful when doing import/export)
+	 *
+	 * e.g. <skinfile(default/myfile.sth)>
+	 * 
+	 * @param	string	$filename	name of file to be inclluded
+	 * @return	void
+	 */
+	public function parse_skinfile($filename)
+	{
+		global $CONF;
+		echo $CONF['SkinsURL'] . Parser::getProperty('IncludePrefix') . $filename;
+		return;
+	}
+
+	/**
+	 * BaseActions::parse_set()
+	 * Sets a property for the parser
+	 * 
+	 * @param	string	$property	name of property
+	 * @param	string	$value		value of property
+	 * @return	void
+	 */
+	public function parse_set($property, $value)
+	{
+		Parser::setProperty($property, $value);
+		return;
+	}
+	
+	/**
+	 * BaseActions::_addIfCondition()
+	 * Helper function: add if condition
+	 * 
+	 * @param	string	$condition	condition for if context
+	 * @return	void
+	 */
+	protected function _addIfCondition($condition)
+	{
+		array_push($this->if_conditions,$condition);
+		$this->_updateTopIfCondition();
+		ob_start();
+		return;
+	}
+	
+	/**
+	 * BaseActions::_updateTopIfCondition()
+	 * Helper function: update the Top of the If Conditions Array
+	 * 
+	 * @param	void
+	 * @return	void
+	 */
+	protected function _updateTopIfCondition()
+	{
+		if ( sizeof($this->if_conditions) == 0 )
+		{
+			$this->if_currentlevel = 1;
+		}
+		else
+		{
+			$this->if_currentlevel = $this->if_conditions[sizeof($this->if_conditions) - 1];
+		}
+		return;
+	}
+	
+	/**
+	 * BaseActions::_addIfExecute()
+	 * Helper function for elseif / elseifnot
+	 * 
+	 * @param	void
+	 * @return	void
+	 */
+	protected function _addIfExecute()
+	{
+		array_push($this->if_execute, 0);
+		return;
+	}
+	
+	/**
+	 * BaseActions::_updateIfExecute()
+	 * Helper function for elseif / elseifnot
+	 * 
+	 * @param	string	$condition	condition to be fullfilled
+	 * @return	void
+	 */
+	protected function _updateIfExecute($condition)
+	{
+		$index = sizeof($this->if_execute) - 1;
+		$this->if_execute[$index] = $this->if_execute[$index] || $condition;
+		return;
+	}
+
+	/**
+	 * BaseActions::_getTopIfCondition()()
+	 * returns the currently top if condition
+	 * 
+	 * @param	void
+	 * @return	string	level
+	 */
+	protected function _getTopIfCondition()
+	{
+		return $this->if_currentlevel;
+	}
+	
+	/**
+	 * BaseActions::setHighlight(()
+	 * Sets the search terms to be highlighted
+	 *
+	 * @param	string	$highlight	A series of search terms
+	 * @return	void
+	 */
+	public function setHighlight($highlight)
+	{
+		$this->strHighlight = $highlight;
+		if ( $highlight )
+		{
+			$this->aHighlight = parseHighlight($highlight);
+		}
+		return;
+	}
+	
+	/**
+	 * BaseActions::highlight()
 	 * Applies the highlight to the given piece of text
 	 *
 	 * @see setHighlight
-	 * @param String	$data	Data that needs to be highlighted
-	 * @return	String	hilighted data
+	 * @param	string	$data		Data that needs to be highlighted
+	 * @return	string	hilighted data
 	 */
-	function highlight($data)
+	public function highlight($data)
 	{
 		if ( $this->aHighlight )
 		{
@@ -227,105 +305,176 @@ class BaseActions
 	}
 	
 	/**
+	 * BaseActions::parse_if()
 	 * Parses <%if%> statements
+	 * 
+	 * @param	void
+	 * @return	void
 	 */
-	function parse_if() {
+	public function parse_if()
+	{
 		$this->_addIfExecute();
-
 		$args = func_get_args();
 		$condition = call_user_func_array(array(&$this,'checkCondition'), $args);
 		$this->_addIfCondition($condition);
+		return;
 	}
 
 	/**
+	 * BaseActions::parse_else()
 	 * Parses <%else%> statements
+	 * 
+	 * @param	void
+	 * @return	void
 	 */
-	function parse_else() {
-		if (sizeof($this->if_conditions) == 0) return;
+	public function parse_else()
+	{
+		if ( sizeof($this->if_conditions) == 0 )
+		{
+			return;
+		}
+		
 		array_pop($this->if_conditions);
-		if ($this->if_currentlevel) {
+		
+		if ( $this->if_currentlevel )
+		{
 			ob_end_flush();
 			$this->_updateIfExecute(1);
 			$this->_addIfCondition(0);
-		} elseif ($this->if_execute[sizeof($this->if_execute) - 1]) {
+		}
+		elseif ( $this->if_execute[sizeof($this->if_execute) - 1] )
+		{
 			ob_end_clean();
 			$this->_addIfCondition(0);
-		} else {
+		}
+		else
+		{
 			ob_end_clean();
 			$this->_addIfCondition(1);
 		}
+		return;
 	}
-
+	
 	/**
+	 * BaseActions::parse_elseif()
 	 * Parses <%elseif%> statements
+	 * 
+	 * @param	void
+	 * @return	void
 	 */
-	function parse_elseif() {
-		if (sizeof($this->if_conditions) == 0) return;
+	public function parse_elseif()
+	{
+		if ( sizeof($this->if_conditions) == 0 )
+		{
+			return;
+		}
+		
 		array_pop($this->if_conditions);
-		if ($this->if_currentlevel) {
+		
+		if ( $this->if_currentlevel )
+		{
 			ob_end_flush();
 			$this->_updateIfExecute(1);
 			$this->_addIfCondition(0);
-		} elseif ($this->if_execute[sizeof($this->if_execute) - 1]) {
+		}
+		elseif ( $this->if_execute[sizeof($this->if_execute) - 1] )
+		{
 			ob_end_clean();
 			$this->_addIfCondition(0);
-		} else {
+		}
+		else
+		{
 			ob_end_clean();
 			$args = func_get_args();
 			$condition = call_user_func_array(array(&$this,'checkCondition'), $args);
 			$this->_addIfCondition($condition);
 		}
+		return;
 	}
-
+	
 	/**
+	 * BaseActions::parse_ifnot()
 	 * Parses <%ifnot%> statements
+	 * 
+	 * @param	void
+	 * @return	void
 	 */
-	function parse_ifnot() {
+	public function parse_ifnot()
+	{
 		$this->_addIfExecute();
-
+		
 		$args = func_get_args();
 		$condition = call_user_func_array(array(&$this,'checkCondition'), $args);
 		$this->_addIfCondition(!$condition);
+		return;
 	}
-
+	
 	/**
+	 * BaseActions::parse_elseifnot()
 	 * Parses <%elseifnot%> statements
+	 * 
+	 * @param	void
+	 * @return	void
 	 */
-	function parse_elseifnot() {
-		if (sizeof($this->if_conditions) == 0) return;
+	public function parse_elseifnot()
+	{
+		if ( sizeof($this->if_conditions) == 0 )
+		{
+			return;
+		}
+		
 		array_pop($this->if_conditions);
-		if ($this->if_currentlevel) {
+		
+		if ( $this->if_currentlevel )
+		{
 			ob_end_flush();
 			$this->_updateIfExecute(1);
 			$this->_addIfCondition(0);
-		} elseif ($this->if_execute[sizeof($this->if_execute) - 1]) {
+		}
+		elseif ( $this->if_execute[sizeof($this->if_execute) - 1] )
+		{
 			ob_end_clean();
 			$this->_addIfCondition(0);
-		} else {
+		}
+		else
+		{
 			ob_end_clean();
 			$args = func_get_args();
 			$condition = call_user_func_array(array(&$this,'checkCondition'), $args);
 			$this->_addIfCondition(!$condition);
 		}
+		return;
 	}
 
 	/**
+	 * BaseActions::parse_endif()
 	 * Ends a conditional if-block
 	 * see e.g. ifcat (BLOG), ifblogsetting (PAGEFACTORY)
+	 * 
+	 * @param	void
+	 * @return	void
 	 */
-	function parse_endif() {
+	public function parse_endif()
+	{
 		// we can only close what has been opened
-		if (sizeof($this->if_conditions) == 0) return;
-
-		if ($this->if_currentlevel) {
+		if ( sizeof($this->if_conditions) == 0 )
+		{
+			return;
+		}
+		
+		if ( $this->if_currentlevel )
+		{
 			ob_end_flush();
-		} else {
+		}
+		else
+		{
 			ob_end_clean();
 		}
+		
 		array_pop($this->if_conditions);
 		array_pop($this->if_execute);
-
+		
 		$this->_updateTopIfCondition();
+		return;
 	}
 }
-?>
