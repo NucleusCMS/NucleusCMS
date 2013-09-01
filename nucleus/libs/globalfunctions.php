ef8d63505f395ed2148e3788621a163fe9fe0c9b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2013-09-01 19:31:25 +0900

    3.65sp1リリースのための修正

--- nucleus/libs/globalfunctions.php
+++ nucleus/libs/globalfunctions.php
@@ -14,7 +14,7 @@
 // needed if we include globalfunctions from install.php
 global $nucleus, $CONF, $DIR_LIBS, $DIR_LANG, $manager, $member;
 
-$nucleus['version'] = 'v3.65';
+$nucleus['version'] = 'v3.65sp1';
 $nucleus['codename'] = '';
 
 // check and die if someone is trying to override internal globals (when register_globals turn on)
