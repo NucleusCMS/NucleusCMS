c33dec90b4a456285aad0aac920a637089648042
yama <yamamoto@kyms.jp>
2014-07-13 11:33:58 +0900

    preg_replace -> str_replace

--- nucleus/libs/globalfunctions.php
+++ nucleus/libs/globalfunctions.php
@@ -381,7 +381,7 @@ $language = getLanguageName();
 # original ereg_replace: ereg_replace( '[\\|/]', '', $language) . '.php')
 # important note that '\' must be matched with '\\\\' in preg* expressions
 
-include($DIR_LANG . preg_replace('#[\\\\|/]#', '', $language) . '.php');
+include($DIR_LANG . str_replace(array('\\','/'), '', $language) . '.php');
 
 /*
     Backed out for now: See http://forum.nucleuscms.org/viewtopic.php?t=3684 for details
@@ -777,7 +777,7 @@ function parseHighlight($query) {
     // TODO: add more intelligent splitting logic
 
     // get rid of quotes
-    $query = preg_replace('/\'|"/', '', $query);
+    $query = str_replace(array("'",'/'), '', $query);
 
     if (!$query) {
         return array();
@@ -1309,7 +1309,7 @@ function selectLanguage($language) {
 	# original ereg_replace: preg_replace( '@\\|/@', '', $language) . '.php')
 	# important note that '\' must be matched with '\\\\' in preg* expressions
 
-	include($DIR_LANG . preg_replace('#[\\\\|/]#', '', $language) . '.php');
+	include($DIR_LANG . str_replace(array('\\','/'), '', $language) . '.php');
 
 }
 
@@ -1442,7 +1442,7 @@ function checkLanguage($lang) {
 	# original ereg_replace: ereg_replace( '[\\|/]', '', $lang) . '.php')
 	# important note that '\' must be matched with '\\\\' in preg* expressions
 
-	return file_exists($DIR_LANG . preg_replace('#[\\\\|/]#', '', $lang) . '.php');
+	return file_exists($DIR_LANG . str_replace(array('\\','/'), '', $lang) . '.php');
 
 }
 
@@ -1459,7 +1459,7 @@ function checkPlugin($plug) {
 	# original ereg_replace: ereg_replace( '[\\|/]', '', $plug) . '.php')
 	# important note that '\' must be matched with '\\\\' in preg* expressions
 
-	return file_exists($DIR_PLUGINS . preg_replace('#[\\\\|/]#', '', $plug) . '.php');
+	return file_exists($DIR_PLUGINS . str_replace(array('\\','/'), '', $plug) . '.php');
 
 }
 
@@ -1944,7 +1944,7 @@ function ticketForPlugin() {
 			# original ereg_replace: ereg_replace( '[\\|/]', '', $language) . '.php')
 			# important note that '\' must be matched with '\\\\' in preg* expressions
 
-			include($DIR_LANG . preg_replace('#[\\\\|/]#', '', $language) . '.php');
+			include($DIR_LANG . str_replace(array('\\','/'), '', $language) . '.php');
 			include($DIR_LIBS . 'PLUGINADMIN.php');
 		}
 
