fd507d01b9cde80035a1bcb674e9598b2af046fb
yama <yamamoto@kyms.jp>
2014-07-19 10:17:40 +0900

    アップデータ - DOCTYPE宣言直前のスペースを削除

--- nucleus/upgrades/upgrade.functions.php
+++ nucleus/upgrades/upgrade.functions.php
@@ -156,11 +156,11 @@
 
 	function upgrade_head() {
 	?>
-			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-			<html xmlns="http://www.w3.org/1999/xhtml">
-			<head>
-				<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-				<title>Nucleus アップグレード</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+	<title>Nucleus アップグレード</title>
 <?php if (file_exists("../styles/manual.css")) { ?>
 				<link rel="stylesheet" href="../styles/manual.css" type="text/css" />
 <?php }else{ ?>
