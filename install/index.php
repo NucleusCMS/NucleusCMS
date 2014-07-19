3ce69251d1450a56d53fe9d556584887ee341cf6
yama <yamamoto@kyms.jp>
2014-07-19 10:15:18 +0900

    インストーラ - DOCTYPE宣言直前のスペースを削除

--- install/index.php
+++ install/index.php
@@ -118,7 +118,7 @@ function showInstallForm() {
 	doCheckFiles();
 
 	?>
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
