a301859388f2fde4e7b9e354767763745cd21458
yama <yamamoto@kyms.jp>
2014-08-18 14:12:26 +0900

    最新バージョンのチェック処理を修正
    3.66と3.7を比べると3.7のほうが古い判定になるため
    (7よりも66のほうが大きいという判定)

--- nucleus/libs/ADMIN.php
+++ nucleus/libs/ADMIN.php
@@ -5321,8 +5321,11 @@ selector();
 				echo '<a href="' . $checkURL . '" title="' . _ADMIN_SYSTEMOVERVIEW_VERSIONCHECK_TITLE . '">Nucleus CMS ' . $nucleus['version'] . $codenamestring . '</a>';
 				$newestVersion = getLatestVersion();
 				$newestCompare = str_replace('/','.',$newestVersion);
+				$newestCompare = floatval($newestCompare);
+				$newestCompare = sprintf('%04.2f', $newestCompare);
 				$currentVersion = str_replace(array('/','v'),array('.',''),$nucleus['version']);
 				$currentVersion = floatval($currentVersion);
+				$currentVersion = sprintf('%04.2f', $currentVersion);
 				if ($newestVersion && version_compare($newestCompare,$currentVersion) > 0) {
 					echo '<br /><a style="color:red" href="'._ADMINPAGEFOOT_OFFICIALURL.'upgrade.php" title="'._ADMIN_SYSTEMOVERVIEW_LATESTVERSION_TITLE.'">'._ADMIN_SYSTEMOVERVIEW_LATESTVERSION_TEXT.$newestVersion.'</a>';
 				}
