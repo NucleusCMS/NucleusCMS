7a297775cfc23af63bc45b368ee03edd0517ee86
yama <yamamoto@kyms.jp>
2014-09-24 17:36:23 +0900

    Small fix

--- nucleus/libs/MANAGER.php
+++ nucleus/libs/MANAGER.php
@@ -393,7 +393,6 @@ class MANAGER {
     {
         if (isset($this->cachedInfo['installedPlugins'])) return;
         
-        $this->cachedInfo['installedPlugins'] = array();
         $res = sql_query('SELECT pid, pfile FROM ' . sql_table('plugin'));
         $this->cachedInfo['installedPlugins'] = array();
         while ($o = sql_fetch_object($res))
