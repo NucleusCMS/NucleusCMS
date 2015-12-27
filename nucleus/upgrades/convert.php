18cd4b289c6b795351cbbc697862a372cc0f22ef
yamamoto <yamamoto@kyms.jp>
2015-12-27 11:15:08 +0900

    convert.php - 実行ボタンをクリックしてから変換実行

--- nucleus/upgrades/convert.php
+++ nucleus/upgrades/convert.php
@@ -1,5 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
+echo '<style>body {line-height:1.7;font-family:Meiryo,\"Hiragino Kaku Gothic Pro\",Arial,\"Helvetica Neue\",Helvetica,sans-serif;} .box {width:500px;margin:5em auto;}</style>';
+if(!isset($_GET['convert'])) {
+    echo '<div class="box">各テーブルの照合順序が utf8_general_ci 以外になっている場合、このスクリプトを実行することで utf8_general_ci に変換できます。旧テーブルは別名をつけてバックアップします。<br /><br />';
+    echo '<a href="?convert=do">変換を実行する</a></div>';
+    exit;
+}
 include_once('upgrade.functions.php');
 
 $srcPrefix = sql_table('');
@@ -9,8 +15,11 @@ $newPrefix = 'temp_'.'nucleus_';
 
 $charset = getCharSetFromDB(sql_table('config'),'name');
 
-if($charset==='utf8') exit('utf8なので問題ありません。何もせずに終了します。');
-
+if($charset==='utf8') exit('<div class="box">utf8なので問題ありません。何もせずに終了します。</div>');
+elseif($charset==='ujis') {
+    $query = sprintf("UPDATE %s SET `value`='japanese-utf8' WHERE `name`='Language'", sql_table('config'));
+    sql_query($query);
+}
 $rs = sql_query("SHOW TABLES LIKE '{$srcPrefix}%'");
 if(!$rs) exit('Nucleusのtableがありません。何もせずに終了します。');
 
