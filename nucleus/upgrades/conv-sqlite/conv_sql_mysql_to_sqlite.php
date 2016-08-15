c6982090331573f4d28552d4f06515d00f67265c
piyoyo <piyoyo@users.noreply.github.com>
2016-08-15 22:33:00 +0900

    New nucleus/upgrades/conv-sqlite/
    (merge from v3.8dev)

---
+++ nucleus/upgrades/conv-sqlite/conv_sql_mysql_to_sqlite.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * convert tool for sqlite
+ *
+ * Copyright (C) 2011-2016 piyoyo
+ */
+
+$this_dir = dirname(__FILE__);
+$parent_dir = dirname(dirname(__FILE__));
+
+if (version_compare(PHP_VERSION, '5.1.0', '<'))
+{
+    fputs(STDERR, "php 5.1.0 or higher needed.\n");
+    exit();
+}
+if (!extension_loaded('pdo') || !extension_loaded('pdo_sqlite'))
+{
+    fputs(STDERR, "pdo or pdo_sqlite not found.\n");
+    exit();
+}
+if (php_sapi_name() != 'cli')
+{
+    fputs(STDERR, "run on cli only.\n");
+    exit();
+}
+
+ob_start();
+$config_php = 'config.php';
+foreach(array('','../','../../../') as $dir)
+    if (is_file("${dir}config.php"))
+    {
+        $config_php = "${dir}config.php";
+        break;
+    }
+include_once($config_php);
+$errors = ob_get_contents();
+ob_end_clean();
+
+//global $CONF;
+//if ($errors && strlen($errors)>0 && $CONF['debug'])
+//    fputs(STDERR, "${errors}\n");
+
+ini_set('display_errors','0');
+//ini_set('display_errors', 'stderr'); // 0, 1, stderr, stdout
+
+if (!in_array("mysql", $MYSQL_HANDLER))
+{
+    echo 'error : $MYSQL_HANDLER is not mysql.' . "\n";
+    exit;
+}
+
+//sql_connect();
+
+//global $DIR_LIBS;
+include($this_dir . DIRECTORY_SEPARATOR . "table_conv_mysql_to_sqlite.php");
+
+$obj = new TableConvertor_mysql_to_sqlite();
+
+//var_dump ($obj->base_table_list);
+
+$tables = array();
+$result = @sql_query(sprintf("SHOW TABLES LIKE '%s'", sql_table("") . "%"));
+while ($row = @sql_fetch_array($result))
+    $tables[] = $row[0];
+
+//$tables = $obj->base_table_list;
+//define('conv_debug', true);
+
+echo "BEGIN;\n\n";
+$error_messages = array();
+foreach ($tables as $table)
+{
+//  echo $table.$obj->get_table_structure($table);
+    @$obj->dump_table_structure($table);
+    echo "\n";
+    @$obj->dump_table_data($table);
+
+    if ($obj->logs_count)
+    {
+        echo "\n/*\n";
+        echo "\n" . implode("\n", $obj->get_logs) . "\n";
+        echo "\n*/\n";
+    }
+    if (defined('conv_debug'))
+    {
+        if (preg_match('/nucleus_plugin_option$/i', $table))
+            exit; // debug
+    }
+}
+echo "\n\nEND;\n";
+
---
+++ nucleus/upgrades/conv-sqlite/readme-ja.txt
@@ -0,0 +1,42 @@
+﻿* convtool
+
+◆自動で変換を行う方法
+
+(1) ルートフォルダのconfig.php: 移行したいサイトのconfig.phpになっているか確認してください。
+
+(2) webからこのフォルダにアクセスします
+
+(3) ログインして変換をします
+
+(4) config.phpの変更 をします
+    $MYSQL_HANDLER = array('pdo','sqlite');
+    if ($MYSQL_HANDLER[1]=='sqlite')
+    {
+       $MYSQL_DATABASE = dirname(__FILE__) . str_replace('/', DIRECTORY_SEPARATOR, '/settings/db_nucleus.sqlite');
+//     $MYSQL_DATABASE = 'pathto/' . 'db_nucleus.sqlite';
+    }
+
+◆手動で行う方法
+・タイムアウトしてしまう場合
+・エラーが出る場合
+・編集したい場合
+は、こちらを推奨。
+
+(1) ルートフォルダのconfig.php: 移行したいサイトのconfig.phpになっているか確認してください。
+
+(2) データベースをsqliteに変換
+php conv_sql_mysql_to_sqlite.php > nucleus-sqlite.sql
+sqlite3 db_nucleus.sqlite < nucleus-sqlite.sql
+または
+php conv_sql_mysql_to_sqlite.php | sqlite3 db_nucleus.sqlite
+
+(3) db_nucleus.sqlite を settingsフォルダに移動します
+
+(4) config.phpの変更
+    $MYSQL_HANDLER = array('pdo','sqlite');
+    if ($MYSQL_HANDLER[1]=='sqlite')
+    {
+       $MYSQL_DATABASE = dirname(__FILE__) . str_replace('/', DIRECTORY_SEPARATOR, '/settings/db_nucleus.sqlite');
+//     $MYSQL_DATABASE = 'pathto/' . 'db_nucleus.sqlite';
+    }
+ 
