31ca45632ebbc9be68b3adedc8f230a97b3e4433
piyoyo <piyoyo@users.noreply.github.com>
2016-12-13 22:14:00 +0900

    add column into item table : Support for public period of the item
    (cherry picked from v3.8dev-ja commit f9f5d48ce10537c7a9ee585990279b1fec9f3b7f)

--- _upgrades/upgrade3.8.php
+++ _upgrades/upgrade3.8.php
@@ -11,9 +11,10 @@
  */
 
 function upgrade_do380() {
+    global $DB_DRIVER_NAME;
 
-	if (upgrade_checkinstall(380))
-		return 'already installed';
+    if (upgrade_checkinstall(380))
+        return 'already installed';
 
     if ( !sql_existTableColumnName(sql_table('blog'), 'bauthorvisible') )
     {
@@ -47,6 +48,28 @@ function upgrade_do380() {
                      ${mode} COLUMN `ballowpast` tinyint(2)   NOT NULL default '1';
                      ", sql_table( 'blog' ));
     upgrade_query('Altering ' . sql_table('blog') . ' table', $query);
+
+    $table_item = sql_table('item');
+    foreach(array( 'ipublic' => "`ipublic` tinyint(2) NOT NULL default '1'",
+                   'ipublic_enable_term_start' => "`ipublic_enable_term_start`  tinyint(2)   NOT NULL default '0'",
+                   'ipublic_enable_term_end'   => "`ipublic_enable_term_end`    tinyint(2)   NOT NULL default '0'",
+                   'ipublic_term_start'        => "`ipublic_term_start`         datetime     NOT NULL default '2000-01-01 00:00:00'",
+                   'ipublic_term_end'          => "`ipublic_term_end`           datetime     NOT NULL default '2099-01-01 00:00:00'"
+        ) as $key=>$value) {
+        if ( !sql_existTableColumnName($table_item, $key) )
+        {
+            $queries = array();
+            $queries[] = sprintf("ALTER TABLE `%s` ADD COLUMN %s", $table_item, $value);
+            if ($DB_DRIVER_NAME == 'sqlite') {
+                $queries[0] .= ';';
+                $queries[] = "CREATE INDEX IF NOT EXISTS `${table_item}_idx_${key}` on `${table_item}` (`${key}`);";
+            } else {
+                $queries[0] .= sprintf(" , ADD INDEX `%s` (`%s`);", $key, $key);
+            }
+            foreach($queries as $query)
+                upgrade_query('Altering ' . $table_item . ' table', $query);
+        }
+    }
 
 	//  -> 3.80
 	// update database version
