24c1860a29bd2aed71d1dc55e7510ce0510ee946
piyopiyobird <36040387+piyopiyobird@users.noreply.github.com>
2018-02-06 22:39:22 +0900

    Fix

--- install/install-mysql.sql
+++ install/install-mysql.sql
@@ -135,7 +135,7 @@ INSERT INTO `nucleus_config` (`name`, `value`) VALUES
     ('SkinsURL',          'http://localhost:8080/nucleus/skins/'),
     ('ActionURL',         'http://localhost:8080/nucleus/action.php'),
     ('URLMode',           'normal'),
-    ('DatabaseName',      'Nucleus');
+    ('DatabaseName',      'Nucleus'),
     ('DatabaseVersion',   '380'),
     ('DebugVars',         '0'),
     ('DefaultListSize',   '10'),
