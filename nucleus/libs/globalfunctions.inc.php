d00d73c021ccff1c15d6150d74b92cba5cd30ae5
piyopiyobird <36040387+piyopiyobird@users.noreply.github.com>
2018-02-24 16:22:00 +0900

    Fix getLatestVersion()

--- nucleus/libs/globalfunctions.inc.php
+++ nucleus/libs/globalfunctions.inc.php
@@ -80,7 +80,7 @@ function getLatestVersion() {
     $options = array('timeout'=> 5, 'connecttimeout'=> 3);
     $ret = Utils::httpGet('http://japan.nucleuscms.org/version_check.php', $options);
 
-    if (empty($ret))
+    if (empty($ret) || !preg_match('@^[0-9\./]+$@ms', $ret))
         $ret = '';
 
     ADMIN::updateConfig('LatestVerText', $ret);
