3304adf01b32e2c5c37c0ca6beefdd84e25a4f0f
piyopiyobird <36040387+piyopiyobird@users.noreply.github.com>
2018-03-03 18:12:00 +0900

    Supports database upgrade using old core system

--- _upgrades/upgrade.functions.php
+++ _upgrades/upgrade.functions.php
@@ -319,17 +319,58 @@ if(!function_exists('sql_existTableColumnName')) {
         }
     }
 
+    if(!function_exists('sql_real_escape_string')) {
+        function sql_real_escape_string($val, $link_identifier = NULL)
+        {
+            if (empty($link_identifier))
+                return mysql_real_escape_string($val);
+            return mysql_real_escape_string($val, $link_identifier);
+        }
+    }
+
     if(!function_exists('sql_fetch_array')) {
         function sql_fetch_array($result, $result_type = MYSQL_BOTH) {
             return mysql_fetch_array($result, $result_type);
         }
     }
 
+    if(!function_exists('sql_fetch_assoc')) {
+        function sql_fetch_assoc($result) {
+            return mysql_fetch_assoc($result);
+        }
+    }
+
+    if(!function_exists('sql_fetch_object')) {
+        function sql_fetch_object($result) {
+            return mysql_fetch_object($result);
+        }
+    }
+
+    if(!function_exists('sql_num_rows')) {
+        function sql_num_rows($result) {
+            return mysql_num_rows($result);
+        }
+    }
+
+    if(!function_exists('sql_error')) {
+        function sql_error($link_identifier = NULL) {
+            if (empty($link_identifier))
+                return mysql_error();
+            return mysql_error($link_identifier);
+        }
+    }
+
+    if(!function_exists('sql_existTableName')) {
+        function sql_existTableName($tablename, $link_identifier = NULL)
+        {
+            $query = sprintf("SHOW TABLES LIKE '%s' ", sql_real_escape_string($tablename));
+            $res = sql_query($query, $link_identifier);
+            return ($res && ($r = sql_fetch_array($res)) && !empty($r)); // PHP(-5.4) Parse error: empty($var = "")  syntax error
+        }
+    }
+
     function sql_getTableColumnNames($tablename)
     {
-        global $MYSQL_CONN, $SQL_DBH;
-        if (!$MYSQL_CONN && !$SQL_DBH) return array();
-
         $sql = sprintf('SHOW COLUMNS FROM `%s` ', $tablename);
         $target = 'Field';
 
