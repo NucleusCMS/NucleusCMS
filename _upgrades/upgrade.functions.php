1cd5545bd0c6e4a58be4041dc9dd60a887128b1d
yamamoto <yamamoto@kyms.jp>
2018-09-28 08:00:06 +0900

    Code cleanup(indent)

--- _upgrades/upgrade.functions.php
+++ _upgrades/upgrade.functions.php
@@ -20,78 +20,78 @@
  *     NOTE: With upgrade to 3.6, need to set this to use sql_* API           *
  **************************************************************/
 
-    if (is_file('../config.php'))
-        include('../config.php');
-    else
-        include('../../config.php');
+if (is_file('../config.php'))
+    include('../config.php');
+else
+    include('../../config.php');
 
-    function load_upgrade_lang() {
-        $_ = getLanguageName();
-        $langNames[] = stripos($_,'japan')!==false ? 'japanese' : $_;
-        $langNames[] = 'english';
-        foreach($langNames as $langName) {
-            $lang_path = dirname(__FILE__) . "/upgrade_lang_{$langName}.php";
-            if(is_file($lang_path)) break;
-            else $lang_path = false;
-        }
-        if($lang_path) include_once($lang_path);
+function load_upgrade_lang() {
+    $_ = getLanguageName();
+    $langNames[] = stripos($_,'japan')!==false ? 'japanese' : $_;
+    $langNames[] = 'english';
+    foreach($langNames as $langName) {
+        $lang_path = dirname(__FILE__) . "/upgrade_lang_{$langName}.php";
+        if(is_file($lang_path)) break;
+        else $lang_path = false;
+    }
+    if($lang_path) include_once($lang_path);
+}
+
+function upgrade_checkinstall($version) {
+    $installed = 0;
+    global $DB_DRIVER_NAME;
+
+    if ($DB_DRIVER_NAME == 'sqlite' && $version<=380)  return true;
+
+    switch($version) {
+        case '300':
+            if (!sql_existTableName(sql_table('config'))) //  < 250
+                return false;
+        default:  // 250 - 380
+            $query = sprintf("SELECT * FROM %s WHERE name='DatabaseVersion' and value>=%d LIMIT 1", sql_table('config'), intval($version));
+            $minrows = 1;
+            break;
     }
 
-    function upgrade_checkinstall($version) {
-        $installed = 0;
-        global $DB_DRIVER_NAME;
-
-        if ($DB_DRIVER_NAME == 'sqlite' && $version<=380)  return true;
-
-        switch($version) {
-            case '300':
-                if (!sql_existTableName(sql_table('config'))) //  < 250
-                    return false;
-            default:  // 250 - 380
-                $query = sprintf("SELECT * FROM %s WHERE name='DatabaseVersion' and value>=%d LIMIT 1", sql_table('config'), intval($version));
-                $minrows = 1;
-                break;
-        }
-
-        $count = 0;
-        $res = sql_query($query);
-        while ( $res && sql_fetch_object($res))
-            $count++;
-        $installed = ($res && ($count >= $minrows));
-        return $installed;
-    }
+    $count = 0;
+    $res = sql_query($query);
+    while ( $res && sql_fetch_object($res))
+        $count++;
+    $installed = ($res && ($count >= $minrows));
+    return $installed;
+}
 
 
-    function upgrade_showLogin($type) {
-        upgrade_head();
-    ?>
-        <h1><?php echo _UPG_TEXT_PLEASE_LOGIN;  ?></h1>
-        <p><?php echo _UPG_TEXT_ENTER_YOUR_DATA;  ?>:</p>
+function upgrade_showLogin($type) {
+    upgrade_head();
+?>
+    <h1><?php echo _UPG_TEXT_PLEASE_LOGIN;  ?></h1>
+    <p><?php echo _UPG_TEXT_ENTER_YOUR_DATA;  ?>:</p>
 
-        <form method="post" action="<?php echo $type?>">
+    <form method="post" action="<?php echo $type?>">
 
-            <ul>
-                <li><?php echo _UPG_TEXT_NAME; ?>: <input name="login" /></li>
-                <li><?php echo _UPG_TEXT_PASSWORD; ?> <input name="password" type="password" /></li>
-            </ul>
+        <ul>
+            <li><?php echo _UPG_TEXT_NAME; ?>: <input name="login" /></li>
+            <li><?php echo _UPG_TEXT_PASSWORD; ?> <input name="password" type="password" /></li>
+        </ul>
 
-            <p>
-                <input name="action" value="login" type="hidden" />
-                <input type="submit" value="<?php echo _UPG_TEXT_LOGIN; ?>" />
-            </p>
+        <p>
+            <input name="action" value="login" type="hidden" />
+            <input type="submit" value="<?php echo _UPG_TEXT_LOGIN; ?>" />
+        </p>
 
-        </form>
-    <?php       upgrade_foot();
-        exit;
-    }
+    </form>
+<?php       upgrade_foot();
+    exit;
+}
 
-    function upgrade_head() {
-        if (_CHARSET != 'UTF-8') {
+function upgrade_head() {
+    if (_CHARSET != 'UTF-8') {
 //            ini_set('default_charset', 'UTF-8');
-            if ( !headers_sent() )
-                header ('Content-Type: text/html; charset=UTF-8');
-        }
-    ?>
+        if ( !headers_sent() )
+            header ('Content-Type: text/html; charset=UTF-8');
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -111,286 +111,185 @@
     <?php
     }
 
-    function upgrade_error($msg) {
-        upgrade_head();
+function upgrade_error($msg) {
+    upgrade_head();
 
-        echo "\n<h1>" . _UPG_TEXT_ERROR_FAILED . "</h1>\n";
-        echo "\n<p>" . _UPG_TEXT_ERROR_WAS . ":</p>\n";
-        echo sprintf("<blockquote><div>%s</div></blockquote>" , $msg);
+    echo "\n<h1>" . _UPG_TEXT_ERROR_FAILED . "</h1>\n";
+    echo "\n<p>" . _UPG_TEXT_ERROR_WAS . ":</p>\n";
+    echo sprintf("<blockquote><div>%s</div></blockquote>" , $msg);
 
-        echo sprintf('<p><a href="index.php" onclick="history.back();">%s</a></p>' , _UPG_TEXT_BACK);
+    echo sprintf('<p><a href="index.php" onclick="history.back();">%s</a></p>' , _UPG_TEXT_BACK);
 
-        upgrade_foot();
-        exit;
+    upgrade_foot();
+    exit;
+}
+
+function upgrade_start() {
+    global $upgrade_failures;
+    $upgrade_failures = 0;
+
+    upgrade_head();
+
+    echo "<h1>" . _UPG_TEXT_EXECUTING_UPGRADES . "</h1>\n<ul>\n";
+
+    if (intGetVar('db_optimize') > 0)
+        upgrade_db_optimize();
+}
+
+function upgrade_end($msg = "") {
+    global $upgrade_failures;
+    $from = intGetVar('from');
+    if ($upgrade_failures > 0)
+        $msg = _UPG_TEXT_QUERIES_HAVE_FAILED_01;
+
+    echo "</ul>\n";
+    echo "<h1>" . _UPG_TEXT_UPGRADE_COMPLETED_TITLE . "</h1>\n";
+    echo "<p>" . $msg . "</p>\n";
+
+    echo sprintf("<p>" . _UPG_TEXT_BACK_TO_OVERVIEW . "</p>\n", "index.php" . ($from>0 ? '?from='.$from : ''));
+
+    upgrade_foot();
+    exit;
+}
+
+/**
+  * Tries to execute a query, gives a message when failed
+  *
+  * @param friendly name
+  * @param query
+  */
+function upgrade_query($friendly, $query) {
+    global $upgrade_failures;
+
+    $friendly = parseQuery($friendly);
+    
+    echo "<li>$friendly ... ";
+    $res = sql_query($query);
+    if (!$res) {
+        echo '<span style="color:red">' . _UPG_TEXT_FAILURE . "</span>\n";
+        echo "<blockquote>" . _UPG_TEXT_REASON_FOR_FAILURE . ": " . sql_error() . " </blockquote>";
+        $upgrade_failures++;
+    } else {
+        echo '<span style="color:green">' . _UPG_TEXT_SUCCESS . "</span><br />\n";
     }
+    echo "</li>";
+    return $res;
+}
 
+/**
+  * Tries to update database version, gives a message when failed
+  *
+  * @param $version
+  *     Schema version the database has been upgraded to
+  */
+function update_version($version) {
+    global $upgrade_failures;
+    $message = "Updating DatabaseVersion in config table to ${version}";
+    if(0==$upgrade_failures){
+        $query = sprintf("UPDATE %s set value='%s' where name='DatabaseVersion'", sql_table('config'), $version);
+        upgrade_query($message, $query);
+    }else
+        echo '<li>'.$message.' ... <span class="warning">NOT EXECUTED</span>\n<blockquote>Errors occurred during upgrade process.</blockquote>';
+}
 
-    function upgrade_start() {
-        global $upgrade_failures;
-        $upgrade_failures = 0;
+/**
+ * @param $table
+ *        table to check (without prefix)
+ * @param $aColumns
+ *        array of column names included
+ */
+function upgrade_checkIfIndexExists($table, $aColumns) {
+    // get info for indices from database
 
-        upgrade_head();
-
-        echo "<h1>" . _UPG_TEXT_EXECUTING_UPGRADES . "</h1>\n<ul>\n";
-
-        if (intGetVar('db_optimize') > 0)
-            upgrade_db_optimize();
-    }
-
-    function upgrade_end($msg = "") {
-        global $upgrade_failures;
-        $from = intGetVar('from');
-        if ($upgrade_failures > 0)
-            $msg = _UPG_TEXT_QUERIES_HAVE_FAILED_01;
-
-        echo "</ul>\n";
-        echo "<h1>" . _UPG_TEXT_UPGRADE_COMPLETED_TITLE . "</h1>\n";
-        echo "<p>" . $msg . "</p>\n";
-
-        echo sprintf("<p>" . _UPG_TEXT_BACK_TO_OVERVIEW . "</p>\n", "index.php" . ($from>0 ? '?from='.$from : ''));
-
-        upgrade_foot();
-        exit;
-    }
-
-    /**
-      * Tries to execute a query, gives a message when failed
-      *
-      * @param friendly name
-      * @param query
-      */
-    function upgrade_query($friendly, $query) {
-        global $upgrade_failures;
-
-        $friendly = parseQuery($friendly);
-        
-        echo "<li>$friendly ... ";
-        $res = sql_query($query);
-        if (!$res) {
-            echo '<span style="color:red">' . _UPG_TEXT_FAILURE . "</span>\n";
-            echo "<blockquote>" . _UPG_TEXT_REASON_FOR_FAILURE . ": " . sql_error() . " </blockquote>";
-            $upgrade_failures++;
-        } else {
-            echo '<span style="color:green">' . _UPG_TEXT_SUCCESS . "</span><br />\n";
+    $aIndices = array();
+    $res = sql_query( sprintf('show index from %s', sql_table($table)) );
+    if ($res)
+    while ($o = sql_fetch_object($res)) {
+        if (!$aIndices[$o->Key_name]) {
+            $aIndices[$o->Key_name] = array();
         }
-        echo "</li>";
-        return $res;
+        $aIndices[$o->Key_name][] = $o->Column_name;
     }
 
-    /**
-      * Tries to update database version, gives a message when failed
-      *
-      * @param $version
-      *     Schema version the database has been upgraded to
-      */
-    function update_version($version) {
-        global $upgrade_failures;
-        $message = "Updating DatabaseVersion in config table to ${version}";
-        if(0==$upgrade_failures){
-            $query = sprintf("UPDATE %s set value='%s' where name='DatabaseVersion'", sql_table('config'), $version);
-            upgrade_query($message, $query);
-        }else
-            echo '<li>'.$message.' ... <span class="warning">NOT EXECUTED</span>\n<blockquote>Errors occurred during upgrade process.</blockquote>';
+    // compare each index with parameter
+    foreach ($aIndices as $keyName => $aIndexColumns) {
+        $aDiff = array_diff($aIndexColumns, $aColumns);
+        if (count($aDiff) == 0) return 1;
     }
 
-    /**
-     * @param $table
-     *        table to check (without prefix)
-     * @param $aColumns
-     *        array of column names included
-     */
-    function upgrade_checkIfIndexExists($table, $aColumns) {
-        // get info for indices from database
+    return 0;
 
-        $aIndices = array();
-        $res = sql_query( sprintf('show index from %s', sql_table($table)) );
-        if ($res)
-        while ($o = sql_fetch_object($res)) {
-            if (!$aIndices[$o->Key_name]) {
-                $aIndices[$o->Key_name] = array();
-            }
-            $aIndices[$o->Key_name][] = $o->Column_name;
-        }
+}
 
-        // compare each index with parameter
-        foreach ($aIndices as $keyName => $aIndexColumns) {
-            $aDiff = array_diff($aIndexColumns, $aColumns);
-            if (count($aDiff) == 0) return 1;
-        }
+/**
+  * Checks to see if a given table exists
+  *
+  * @param $table
+  *     Name of table to check for existance of
+  *     Uses sql_table internally
+  * @return true if table exists, false otherwise.
+  */
+function upgrade_checkIfTableExists($table){
+    $res = sql_query( sprintf("SHOW TABLES LIKE '%s'", sql_table($table)) );
+    return ($res != 0) && (sql_num_rows($res) == 1);
+}
 
-        return 0;
+/**
+  * Checks to see if a given configuration value exists
+  *
+  * @param $name
+  *     Config value to check for existance of.
+  *     Paramater must be MySQL escaped
+  * @return true if configuration value exists, false otherwise.
+  */
+function upgrade_checkIfCVExists($name){
+    $res = sql_query( sprintf("SELECT name from %s WHERE name='%s'", sql_table('config'), $name) );
+    return ($res != 0) && (sql_num_rows($res) == 1);
+}
 
-    }
+/**
+  * Checks to see if a given column exists
+  *
+  * @param $table
+  *     Name of table to check for column in
+  *     Uses sql_table internally
+  * @param $col
+  *     Name of column to check for existance of
+  * @return true if column exists, false otherwise.
+  */
+function upgrade_checkIfColumnExists($table, $col){
+    $res = sql_query( sprintf('DESC `%s` `%s`', sql_table($table), $col) );
+    return ($res != 0) && (sql_num_rows($res) == 1);
+}
 
-    /**
-      * Checks to see if a given table exists
-      *
-      * @param $table
-      *     Name of table to check for existance of
-      *     Uses sql_table internally
-      * @return true if table exists, false otherwise.
-      */
-    function upgrade_checkIfTableExists($table){
-        $res = sql_query( sprintf("SHOW TABLES LIKE '%s'", sql_table($table)) );
-        return ($res != 0) && (sql_num_rows($res) == 1);
-    }
+function upgrade_db_optimize()
+{
+    global $DB_DRIVER_NAME;
+    if ($DB_DRIVER_NAME == 'mysql')
+        upgrade_db_optimize_mysql();
+}
 
-    /**
-      * Checks to see if a given configuration value exists
-      *
-      * @param $name
-      *     Config value to check for existance of.
-      *     Paramater must be MySQL escaped
-      * @return true if configuration value exists, false otherwise.
-      */
-    function upgrade_checkIfCVExists($name){
-        $res = sql_query( sprintf("SELECT name from %s WHERE name='%s'", sql_table('config'), $name) );
-        return ($res != 0) && (sql_num_rows($res) == 1);
-    }
+function upgrade_db_optimize_mysql()
+{
+    global $DB_DRIVER_NAME;
+    if ($DB_DRIVER_NAME != 'mysql')
+        return;
 
-    /**
-      * Checks to see if a given column exists
-      *
-      * @param $table
-      *     Name of table to check for column in
-      *     Uses sql_table internally
-      * @param $col
-      *     Name of column to check for existance of
-      * @return true if column exists, false otherwise.
-      */
-    function upgrade_checkIfColumnExists($table, $col){
-        $res = sql_query( sprintf('DESC `%s` `%s`', sql_table($table), $col) );
-        return ($res != 0) && (sql_num_rows($res) == 1);
-    }
+    $tables = array();
+    $res = sql_query(sprintf("SHOW TABLE STATUS LIKE '%s%%'", sql_table('')));
+    while ($res && ($row = sql_fetch_assoc($res)) && !empty($row))
+        $tables[] = $row['Name'];
 
-    function upgrade_db_optimize()
+    if (count($tables)>0)
     {
-        global $DB_DRIVER_NAME;
-        if ($DB_DRIVER_NAME == 'mysql')
-            upgrade_db_optimize_mysql();
-    }
-
-    function upgrade_db_optimize_mysql()
-    {
-        global $DB_DRIVER_NAME;
-        if ($DB_DRIVER_NAME != 'mysql')
-            return;
-
-        $tables = array();
-        $res = sql_query(sprintf("SHOW TABLE STATUS LIKE '%s%%'", sql_table('')));
-        while ($res && ($row = sql_fetch_assoc($res)) && !empty($row))
-            $tables[] = $row['Name'];
-
-        if (count($tables)>0)
-        {
-            foreach(array('REPAIR', 'OPTIMIZE') as $cmd) {
-                $sql = $cmd . " TABLE `" . implode("`, `", $tables) . "`";
-                $res = upgrade_query($cmd . ' TABLE', $sql);
-                while($res && ($row = sql_fetch_assoc($res)))
-                {  }
-                if ($res)
-                    sql_free_result($res);
-            }
+        foreach(array('REPAIR', 'OPTIMIZE') as $cmd) {
+            $sql = $cmd . " TABLE `" . implode("`, `", $tables) . "`";
+            $res = upgrade_query($cmd . ' TABLE', $sql);
+            while($res && ($row = sql_fetch_assoc($res)))
+            {  }
+            if ($res)
+                sql_free_result($res);
         }
-    }
-
-if(!function_exists('sql_existTableColumnName')) {
-    function sql_existTableColumnName($tablename, $ColumnName, $casesensitive=false)
-    {
-        $names = sql_getTableColumnNames($tablename);
-
-        if (count($names)>0)
-        {
-            if ($casesensitive)
-                return in_array( $ColumnName , $names );
-            else
-            {
-                foreach($names as $v)
-                    if ( strcasecmp( $ColumnName , $v ) == 0 )
-                    {
-                         return true;
-                    }
-            }
-        }
-        return false;
-    }
-
-    if(!function_exists('sql_query')) {
-        function sql_query($query, $link_identifier =NULL) {
-            return mysql_query($query, $link_identifier);
-        }
-    }
-
-    if(!function_exists('sql_real_escape_string')) {
-        function sql_real_escape_string($val, $link_identifier = NULL)
-        {
-            if (empty($link_identifier))
-                return mysql_real_escape_string($val);
-            return mysql_real_escape_string($val, $link_identifier);
-        }
-    }
-
-    if(!function_exists('sql_fetch_array')) {
-        function sql_fetch_array($result, $result_type = MYSQL_BOTH) {
-            return mysql_fetch_array($result, $result_type);
-        }
-    }
-
-    if(!function_exists('sql_fetch_assoc')) {
-        function sql_fetch_assoc($result) {
-            return mysql_fetch_assoc($result);
-        }
-    }
-
-    if(!function_exists('sql_fetch_object')) {
-        function sql_fetch_object($result) {
-            return mysql_fetch_object($result);
-        }
-    }
-
-    if(!function_exists('sql_num_rows')) {
-        function sql_num_rows($result) {
-            return mysql_num_rows($result);
-        }
-    }
-
-    if(!function_exists('sql_error')) {
-        function sql_error($link_identifier = NULL) {
-            if (empty($link_identifier))
-                return mysql_error();
-            return mysql_error($link_identifier);
-        }
-    }
-
-    if(!function_exists('sql_existTableName')) {
-        function sql_existTableName($tablename, $link_identifier = NULL)
-        {
-            $query = sprintf("SHOW TABLES LIKE '%s' ", sql_real_escape_string($tablename));
-            $res = sql_query($query, $link_identifier);
-            return ($res && ($r = sql_fetch_array($res)) && !empty($r)); // PHP(-5.4) Parse error: empty($var = "")  syntax error
-        }
-    }
-
-    function sql_getTableColumnNames($tablename)
-    {
-        $sql = sprintf('SHOW COLUMNS FROM `%s` ', $tablename);
-        $target = 'Field';
-
-        $items = array();
-        $res = sql_query($sql);
-        if (!$res)
-            return array();
-        while( $row = sql_fetch_array($res) )
-        {
-            if (isset($row[$target]))
-                $items[] = $row[$target];
-        }
-
-        if (count($items)>0)
-        {
-            sort($items);
-        }
-        return $items;
     }
 }
 
