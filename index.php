2143d330e238745ea2dbad04f37efbf56e333824
yama <yamamoto@kyms.jp>
2020-05-26 21:31:27 +0900

    Code cleanup

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 $CONF = array();
 $CONF['Self'] = 'index.php';
 
-if (!@is_file('./config.php')) {
-    if (@is_file('./install/index.php') && !headers_sent()) {
+if (!is_file('./config.php')) {
+    if (is_file('./install/index.php') && !headers_sent()) {
         header('Location: ./install/');
         exit;
     }
-    $f = dirname(__FILE__).'/nucleus/libs/config-error.php';
-    if (@is_file($f)) @include($f);
+    $f = __DIR__ .'/nucleus/libs/config-error.php';
+    if (is_file($f)) @include($f);
     header('Content-type: text/html; charset=utf-8');
     echo '<h1>Configuration error</h1>';
     echo '<p>please run the <a href="./install/index.php">install script</a> or modify config.php</p>';
