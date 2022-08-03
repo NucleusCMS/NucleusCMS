d431747953b9c62b6142b01a2f76bb065116ab2b
torisanbird <110459848+torisanbird@users.noreply.github.com>
2022-08-03 21:30:00 +0900

    add php wrapper functions : str_contains, str_starts_with, str_ends_with

--- nucleus/libs/globalfunctions.inc.php
+++ nucleus/libs/globalfunctions.inc.php
@@ -2699,19 +2699,52 @@ function checkOutputCompression($content_type)
     }
 }
 
-function str_contains($haystack, $needle)
-{
-    $pos = strpos($haystack, $needle);
-    if ($pos !== false) {
-        return true;
-    }
-
-    return false;
-}
-
 function str_contain($haystack, $needle)
 {
     return str_contains($haystack, $needle);
+}
+
+if (!function_exists('str_contains')) {
+    // str_contains [PHP8 - ] : for PHP5, PHP7 / ext/standard/string.c php_memnstr
+    // Note: This function returns true if the needle string is empty.
+    function str_contains(string $haystack, string $needle) {
+        return strpos($haystack, $needle) !== false; // don't localize
+        // result = bool(true)  : php -nr "var_dump(str_contains('',''));"
+        // result = bool(true)  : php -nr "var_dump(str_contains('0',''));"
+        // result = bool(false) : php -nr "var_dump(str_contains('','0'));"
+        // result = bool(true)  : php -nr "$haystack='';$needle='';var_dump(strpos($haystack, $needle) !== false);"
+        // result = bool(true)  : php -nr "$haystack='0';$needle='';var_dump(strpos($haystack, $needle) !== false);"
+        // result = bool(false) : php -nr "$haystack='';$needle='0';var_dump(strpos($haystack, $needle) !== false);"
+    }
+}
+
+if (!function_exists('str_starts_with')) {
+    // str_contains [PHP8 - ] : for PHP5, PHP7 / ext/standard/string.c zend_string_starts_with 
+    // Note: This function returns true if the needle string is empty.
+    function str_starts_with(string $haystack, string $needle) {
+        return strncmp($haystack, $needle, strlen($needle)) === 0; // don't localize
+        // result = bool(true)  : php -nr "var_dump(str_starts_with('',''));"
+        // result = bool(true)  : php -nr "var_dump(str_starts_with('0',''));"
+        // result = bool(false) : php -nr "var_dump(str_starts_with('','0'));"
+        // result = bool(true)  : php -nr "var_dump(strncmp('','', 0) === 0);"
+        // result = bool(true)  : php -nr "var_dump(strncmp('0','', 0) === 0);"
+        // result = bool(false) : php -nr "var_dump(strncmp('','0', 1) === 0);"
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    // str_contains [PHP8 - ] : for PHP5, PHP7 / ext/standard/string.c if len , memcmp
+    // Note: This function returns true if the needle string is empty.
+    function str_ends_with(string $haystack, string $needle) {
+        if (strlen($needle) > strlen($haystack)) {
+            return false; // don't localize
+        }
+        return strlen($needle) === 0 || substr($haystack, -strlen($needle)) === $needle;
+        // result = bool(true)  : php -nr "var_dump(str_ends_with('',''));"
+        // result = bool(true)  : php -nr "var_dump(str_ends_with('0',''));"
+        // result = bool(true)  : php -nr "var_dump(str_ends_with('1',''));"
+        // result = bool(false) : php -nr "var_dump(str_ends_with('','0'));"
+    }
 }
 
 function getBaseUrl()
