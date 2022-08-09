50580e599bccbaddf9e89c53e7f353ab764cda4a
torisanbird <110459848+torisanbird@users.noreply.github.com>
2022-08-09 20:00:12 +0900

    コード整理 PHP_VERSION_ID

--- nucleus/libs/globalfunctions.php
+++ nucleus/libs/globalfunctions.php
@@ -41,7 +41,7 @@ if (version_compare(phpversion(), '5.3.0', '<')) {
     exit();
 }
 
-define('HAS_CATCH_ERROR', version_compare('7.0.0', PHP_VERSION, '<='));
+define('HAS_CATCH_ERROR', 70000 <= PHP_VERSION_ID);
 
 // needed if we include globalfunctions from install.php
 global $nucleus, $CONF, $DIR_LIBS, $DIR_LANG, $manager, $member;
@@ -67,7 +67,7 @@ if (!defined('DEFAULT_USER_AGENT')) {
 }
 ini_set('user_agent', DEFAULT_USER_AGENT);
 
-if (version_compare(phpversion(), '5.4.0', '<')) {
+if (PHP_VERSION_ID < 50400) { // [ - 5.3]
     if (ini_get('register_globals')) {
         exit('Should be change off register_globals.');
     }
