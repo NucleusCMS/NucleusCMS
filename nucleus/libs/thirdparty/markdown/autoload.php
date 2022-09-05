5e8d2625fc455e7445a5412579a5ef90a8bee8d2
torisanbird <110459848+torisanbird@users.noreply.github.com>
2022-09-05 22:28:41 +0900

    Refactor

--- nucleus/libs/thirdparty/markdown/autoload.php
+++ nucleus/libs/thirdparty/markdown/autoload.php
@@ -3,13 +3,13 @@
 if (!class_exists('\cebe\markdown\Markdown')) {
     spl_autoload_register(
         function ($className) {
-        $m = [];
-        if (preg_match('/^\\\\?cebe\\\\markdown((?:\\\\[\w]+){1,3})$/', $className, $m)) {
-            $f = __DIR__ . "/{$m[1]}.php";
-            if (@is_file($f)) {
-                include_once($f);
+            $m = [];
+            if (preg_match('/^\\\\?cebe\\\\markdown((?:\\\\[\w]+){1,3})$/', $className, $m)) {
+                $f = __DIR__ . "/{$m[1]}.php";
+                if (@is_file($f)) {
+                    include_once($f);
+                }
             }
         }
-    }
     );
 }
