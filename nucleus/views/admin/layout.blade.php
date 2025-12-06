efa86f3b7e42fd929e609954341abe69a3f234c8
yamamoto <yamamoto@kyms.jp>
2025-12-06 23:22:39 +0900

    Improve: 管理画面のデザインをモダンに刷新

--- nucleus/views/admin/layout.blade.php
+++ nucleus/views/admin/layout.blade.php
@@ -4,13 +4,26 @@
 {!! $head !!}
 </head>
 <body>
-    <div id="adminwrapper">
-        <div class="header">
-            <h1>{{ $SiteName }}</h1>
-        </div>
-        <div id="container">
-            <div id="content">
+    <div class="app-shell">
+        <header class="app-header">
+            <div class="brand">
+                <div class="brand-mark">N</div>
+                <div class="brand-text">
+                    <div class="brand-name">{{ $SiteName }}</div>
+                    <div class="brand-subtitle">My Nucleus CMS</div>
+                </div>
+            </div>
+            <div class="header-meta">
                 @php $oAdmin->loginname(); @endphp
+            </div>
+        </header>
+        <div class="app-body">
+            <aside class="app-sidebar">
+                <div class="sidebar-inner">
+                    @php $oAdmin->quickmenu(); @endphp
+                </div>
+            </aside>
+            <main id="content" class="app-content">
                 @if ($oAdmin->hasSystemInfoMessages())
                     <div class="system-info-messages">
                         @foreach ($oAdmin->getSystemInfoMessages() as $info)
@@ -19,9 +32,11 @@
                     </div>
                 @endif
                 {!! $content !!}
-            </div>
-            {!! $foot !!}
+            </main>
         </div>
+        <footer class="app-footer">
+            {!! $foot !!}
+        </footer>
     </div>
 </body>
 </html>
