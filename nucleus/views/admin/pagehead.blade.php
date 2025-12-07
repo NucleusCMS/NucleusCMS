53ce4d203fe44d76f47f41bffdb1c39fcc6f08c5
yamamoto <yamamoto@kyms.jp>
2025-12-07 17:21:32 +0900

    Remove redundant datepicker CSS link for Japanese locale

--- nucleus/views/admin/pagehead.blade.php
+++ nucleus/views/admin/pagehead.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="{{ $baseUrl }}styles/jquery-ui/jquery-ui.theme.min.css">
     <link rel="stylesheet" href="{{ $baseUrl }}styles/jquery-ui/jquery-ui.structure.min.css">
     @if (_LOCALE == 'ja_JP')
-        <link rel="stylesheet" href="{{ $baseUrl }}styles/jquery-ui/ui.datepicker-ja.css">
         <script src="{{ $baseUrl }}javascript/jquery/i18n/jquery.ui.datepicker-ja.js"></script>
     @endif
     <script src="{{ $baseUrl }}javascript/jquery/jquery-ui.min.js"></script>
