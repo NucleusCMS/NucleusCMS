3afad5d48a2d6b4985c5dcc6a09dcb22a5d718ac
yamamoto <yamamoto@kyms.jp>
2025-12-11 20:23:21 +0900

    PHP8文字列関数のポリフィルで7.4互換性を維持

--- nucleus/libs/phpfunctions.php
+++ nucleus/libs/phpfunctions.php
@@ -30,3 +30,31 @@ if ( ! function_exists('each')) { // removed function PHP[ - 7.4]
         return [1 => $value, 'value' => $value, 0 => $key, 'key' => $key];
     }
 }
+
+if ( ! function_exists('str_contains')) { // added PHP 8
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return '' === $needle || false !== strpos($haystack, $needle);
+    }
+}
+
+if ( ! function_exists('str_starts_with')) { // added PHP 8
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        $needle_length = strlen($needle);
+        return 0 === $needle_length || 0 === strncmp($haystack, $needle, $needle_length);
+    }
+}
+
+if ( ! function_exists('str_ends_with')) { // added PHP 8
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        $needle_length = strlen($needle);
+        if (0 === $needle_length) {
+            return true;
+        }
+
+        return $needle_length <= strlen($haystack)
+            && substr($haystack, -$needle_length) === $needle;
+    }
+}
