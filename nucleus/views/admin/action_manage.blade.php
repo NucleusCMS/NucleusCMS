56fbae5db0434680c277eafe3852fdb932527d5b
yamamoto <yamamoto@kyms.jp>
2025-12-20 19:04:22 +0900

    管理トップからメンバー管理とリンクセクションを削除

--- nucleus/views/admin/action_manage.blade.php
+++ nucleus/views/admin/action_manage.blade.php
@@ -10,15 +10,9 @@
 <div style='float:left; padding: 0 1em;'>
     <h2>{{ _MANAGE_GENERAL }}</h2>
     <ul>
-        <li><a href="index.php?action=usermanagement">{{ _OVERVIEW_MEMBERS }}</a></li>
         <li><a href="index.php?action=settingsedit">{{ _OVERVIEW_SETTINGS }}</a></li>
         <li><a href="index.php?action=systemoverview">{{ _QMENU_MANAGE_SYSTEM }}</a></li>
         <li><a href="index.php?action=actionlog">{{ _OVERVIEW_VIEWLOG }}</a></li>
         <li><a href="index.php?action=systemlog">{{ _SYSTEMLOG_TITLE }}</a></li>
     </ul>
-</div>
-
-<div style='float:none; clear: both; padding: 0 1em;'>
-    <h2>{{ _LINKS }}</h2>
-    <ul>{!! preg_replace("#<a #","<a target='_blank' rel='noreferrer' ", $_MANAGE_LINKS_ITEMS) !!}</ul>
 </div>
