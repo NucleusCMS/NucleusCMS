beecbc28441f2db0549a371e793eed19370eb165
yamamoto <yamamoto@kyms.jp>
2025-12-21 17:03:27 +0900

    サイドバーに収まる短い管理メニューラベルに調整

--- nucleus/language/french-utf8.php
+++ nucleus/language/french-utf8.php
@@ -601,7 +601,7 @@ try_define('_QMENU_ADD_SELECT',		'-- sélectionnez --');
 try_define('_QMENU_USER_SETTINGS',		'Préférences');
 try_define('_QMENU_USER_ITEMS',		'Billets');
 try_define('_QMENU_USER_COMMENTS',		'Commentaires');
-try_define('_QMENU_MANAGE',			'Gestion & paramètres');
+try_define('_QMENU_MANAGE',			'Gestion');
 try_define('_QMENU_MANAGE_LOG',		'Log');
 try_define('_QMENU_MANAGE_SETTINGS',	'Configuration');
 try_define('_QMENU_MANAGE_MEMBERS',		'Participants');
--- nucleus/language/german-utf8.php
+++ nucleus/language/german-utf8.php
@@ -270,7 +270,7 @@ define('_QMENU_ADD_SELECT',			'-- ausw&auml;hlen --');
 define('_QMENU_USER_SETTINGS',		'Einstellungen');
 define('_QMENU_USER_ITEMS',			'Artikel');
 define('_QMENU_USER_COMMENTS',		'Kommentare');
-define('_QMENU_MANAGE',				'Verwaltung & Einstellungen');
+define('_QMENU_MANAGE',				'Verwaltung');
 define('_QMENU_MANAGE_LOG',			'Logdatei');
 define('_QMENU_MANAGE_SETTINGS',	'Konfiguration');
 define('_QMENU_MANAGE_MEMBERS',		'Benutzer');
